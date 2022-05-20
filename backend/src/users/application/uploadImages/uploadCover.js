4a87593fa56ca0db1e042604777885de63194c3f
jorge <lower.joerge.com>
2022-05-19 20:55:10 -0600

    add(feat): subir imagenes a tu perfil

---
+++ backend/src/users/application/uploadImages/uploadCover.js
@@ -0,0 +1,40 @@
+const { uploadImage } = require("../../../libs/cloudynary")
+const User = require("../../domain/UserModel")
+const fs = require('fs-extra')
+
+const UploadCover = async (req, res)=>{
+
+  const user = await User.findById(req.query.userId)
+  
+  if(!user){
+    res.status(404).json({message:"user no encontrado"})
+  }
+
+  try {
+    
+      if(!req.files.image){
+         res.status(404).json({message:"envio un archivo invalido"})
+        
+        }
+        else{
+           const result = await uploadImage(req.files.image.tempFilePath)  
+          await fs.remove(req.files.image.tempFilePath)
+          const imageCover =  {
+              url: result.secure_url, 
+              public_id:result.public_id
+            }
+          user.coverPicture = imageCover
+    
+          await user.save()
+    
+          res.status(202).json({message:"se ha agregado una foto"})
+        }
+  } catch (error) {
+
+    return res.status(500).json({message:"algo salio mal"})
+    
+  }
+
+}
+
+module.exports = UploadCover
