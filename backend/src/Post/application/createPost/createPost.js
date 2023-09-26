1394254d1cd5baf2b2725bf45b19bb9eb12afa8d
JorgeLap <jorgais.ernesto@gmail.com>
2023-09-26 13:29:12 -0700

    add feature to upload a post

--- backend/src/Post/application/createPost/createPost.js
+++ backend/src/Post/application/createPost/createPost.js
@@ -1,64 +1,45 @@
-const User = require('../../../users/domain/UserModel')
-const Post = require("../../dominio/Post")
-const Feeling = require('../../dominio/Feeling')
+const User = require("../../../users/domain/UserModel");
+const Post = require("../../dominio/Post");
 
+const createImagen = require("./createImagen");
+const createVotes = require("./createVotes");
 
-const createImagen = require('./createImagen')
-const createVotes = require('./createVotes')
+const createPost = async (req, res) => {
+  const { description, userId, votes, difusion, postShared } = req.body;
 
+  let Votes = [];
 
-const createPost = async (req, res)=>{
-  const {description, userId, votes, feeling, postShared} = req.body
-
-
-  
-  let Votes = []
-  let feelingId = null
- 
   try {
-    const user = await User.findById(userId)
-    if(!user) {
-      return res.status(500).json({message:'user no proporcionado'})
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(500).json({ message: "user no proporcionado" });
     }
     let image = null;
-   
+
     if (req.files?.image) {
-     image = await createImagen(req)
-    }
-   
-    if(feeling){
-      feelingId = await Feeling.findOne({feeling:feeling})
+      image = await createImagen(req);
     }
 
-
-    if(votes){
-        Votes = await createVotes(req, res)
-       
+    if (votes) {
+      Votes = await createVotes(req, res);
     }
-    
 
-    const newPost = new Post({ userId, description, image});
-    const votesId = Votes.map(vote => vote._id)
+    const newPost = new Post({ userId, description, image });
+    const votesId = Votes.map((vote) => vote._id);
 
-    feelingId  ? newPost.feeling  = feelingId._id : 0
-    if(postShared){
-      newPost.postShared = postShared
+    if (postShared) {
+      newPost.postShared = postShared;
     }
-    newPost.votes = votesId
-    user.posts = [...user.posts, newPost]
-   
-    await user.save()
+    newPost.votes = votesId;
+    user.posts = [...user.posts, newPost];
+
+    await user.save();
     await newPost.save();
-    
-    return res.status(201).json(newPost)
+
+    return res.status(201).json(newPost);
+  } catch (error) {
+    return res.status(500).json({ message: "algo salio posts" });
   }
-  catch (error) {
+};
 
-    return res.status(500).json({message:"algo salio posts"})
-  }
-
-  
-
-}
-
-module.exports = createPost
+module.exports = createPost;
