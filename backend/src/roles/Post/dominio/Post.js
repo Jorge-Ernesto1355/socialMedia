e1f5b6aa0c069dc4c0ecd03e4161bbff35005fae
JorgeLap <jorgais.ernesto@gmail.com>
2023-09-26 13:29:38 -0700

    delete feelings to post

--- backend/src/roles/Post/dominio/Post.js
+++ backend/src/roles/Post/dominio/Post.js
@@ -1,41 +1,42 @@
-const {Schema,model} = require('mongoose')
+const { Schema, model } = require("mongoose");
 
-
-const Post = Schema({
-  userId:{type:String},
-  description:{ type:String, max:500},
-  image:{url:String, public_id:String},
-  likes:{type:Array, default:[]},
-  comments:[{
-     ref:'Comment', 
-     type:Schema.Types.ObjectId
-  }],
-  shares:{type:Array, default:[]}, 
-  votes:[
+const Post = Schema(
+  {
+    userId: { type: String },
+    description: { type: String, max: 500 },
+    image: { url: String, public_id: String },
+    likes: { type: Array, default: [] },
+    comments: [
       {
-        ref:"Vote", 
-        type:Schema.Types.ObjectId, 
-        
-      }
-    ], 
-  feeling:{
-    ref:"Feeling", 
-    type:Schema.Types.ObjectId
-  }, 
-  postShared:{ref:'Post', type:Schema.Types.ObjectId}, 
-
- actions:[
+        ref: "Comment",
+        type: Schema.Types.ObjectId,
+      },
+    ],
+    shares: { type: Array, default: [] },
+    votes: [
       {
-        ref:"Action", 
-        type:Schema.Types.ObjectId, 
-        
-      }
-    ], 
-  group:{type:String, default:""}, 
-  edit:{type:Boolean, default:false}
+        ref: "Vote",
+        type: Schema.Types.ObjectId,
+      },
+    ],
+    feeling: {
+      ref: "Feeling",
+      type: Schema.Types.ObjectId,
+    },
+    postShared: { ref: "Post", type: Schema.Types.ObjectId },
 
-}, {
-   timestamps:true
-})
+    actions: [
+      {
+        ref: "Action",
+        type: Schema.Types.ObjectId,
+      },
+    ],
+    group: { type: String, default: "" },
+    edit: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = model('Post', Post)
+module.exports = model("Post", Post);
