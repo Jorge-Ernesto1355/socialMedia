1527541092312d2a29f2a05e52869a4730e28589
JorgeLap <jorgais.ernesto@gmail.com>
2023-09-26 13:35:30 -0700

    add functions to component difusions

--- client/src/zustand/CreatePostStore.js
+++ client/src/zustand/CreatePostStore.js
@@ -3,7 +3,7 @@ import { create } from "zustand";
 const CreatePostStore = create((set, get) => ({
   text: "",
   votes: [],
-
+  difusion: "",
   setText: (text) => set({ text: text() }),
   addVotes: (votes) => {
     if (!votes) return null;
@@ -11,6 +11,11 @@ const CreatePostStore = create((set, get) => ({
   },
   delVotes: () => {
     set({ votes: [] });
+  },
+
+  setDifusion: (difusion) => {
+    if (!difusion) return;
+    set({ difusion });
   },
 }));
 
