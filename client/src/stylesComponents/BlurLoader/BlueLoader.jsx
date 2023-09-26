f1d0d8fe742050059dfa5a84f1a68facffb1dab1
JorgeLap <jorgais.ernesto@gmail.com>
2023-09-26 13:34:02 -0700

    add feature blur to upload post isLoading

---
+++ client/src/stylesComponents/BlurLoader/BlueLoader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoaderPost from '../LoaderPost/LoaderPost'
+import './blurLoader.css'
+const BlueLoader = () => {
+  return (
+    <div  className='blurLoader-container'>
+            <LoaderPost/>
+    </div>
+  )
+}
+
+export default BlueLoader
