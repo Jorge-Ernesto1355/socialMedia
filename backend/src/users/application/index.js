e3140f1124c036f4d7e491fd576c38752328c895
JorgeLap <jorgais.ernesto@gmail.com>
2023-09-28 12:36:04 -0700

    add new request to userIndex

--- backend/src/users/application/index.js
+++ backend/src/users/application/index.js
@@ -1,37 +1,36 @@
-
-const FindAllUsers = require('./find/FindAllUsers')
-const FindUserById = require('./find/FindUserById')
-const FindAllFollowers = require('./find/FindAllFollowers')
-const FindUserPosts = require('./find/FindUserPosts')
-const AcceptFriends = require('./makeFriends/friends/AcceptFriends')
-const addFriend = require('./makeFriends/friends/AddFriends')
-const deleteFriend  = require('../application/makeFriends/friends/deleteFriends')
-const UploadProfilePicture = require('../application/uploadImages/uploadProfilePicture')
-const UploadCover = require('../application/uploadImages/uploadCover')
-const addRelationShip = require('../application/makeFriends/relationship/addRelationShip')
-const AcceptRelationShip = require('../application/makeFriends/relationship/AcceptRelationShip')
-const deleteRelationShip = require('../application/makeFriends/relationship/deleteRelationShip')
-const giveRoles = require('../../roles/application/giveRoles')
-const deleteRoles = require('../../roles/application/removeRoles')
-const FindUsersActions = require('../application/find/FindUsersActions')
+const FindAllUsers = require("./find/FindAllUsers");
+const FindUserById = require("./find/FindUserById");
+const FindAllFollowers = require("./find/FindAllFollowers");
+const FindUserPosts = require("./find/FindUserPosts");
+const AcceptFriends = require("./makeFriends/friends/AcceptFriends");
+const addFriend = require("./makeFriends/friends/AddFriends");
+const deleteFriend = require("../application/makeFriends/friends/deleteFriends");
+const UploadProfilePicture = require("../application/uploadImages/uploadProfilePicture");
+const UploadCover = require("../application/uploadImages/uploadCover");
+const addRelationShip = require("../application/makeFriends/relationship/addRelationShip");
+const AcceptRelationShip = require("../application/makeFriends/relationship/AcceptRelationShip");
+const deleteRelationShip = require("../application/makeFriends/relationship/deleteRelationShip");
+const giveRoles = require("../../roles/application/giveRoles");
+const deleteRoles = require("../../roles/application/removeRoles");
+const FindUsersActions = require("../application/find/FindUsersActions");
+const getRequestFriends = require("../application/makeFriends/friends/getRequestFriend");
 
 module.exports = {
-FindUserPosts,
-FindAllUsers, 
-FindUserById,
-FindAllFollowers, 
-AcceptFriends, 
-addFriend, 
-deleteFriend, 
-UploadCover,
-UploadProfilePicture,
-addRelationShip,
-deleteRelationShip, 
-AcceptRelationShip , 
-giveRoles,
-deleteRoles,
-FindUserPosts, 
-FindUsersActions
-}
-
-
+  FindUserPosts,
+  FindAllUsers,
+  FindUserById,
+  FindAllFollowers,
+  AcceptFriends,
+  addFriend,
+  deleteFriend,
+  UploadCover,
+  UploadProfilePicture,
+  addRelationShip,
+  deleteRelationShip,
+  AcceptRelationShip,
+  giveRoles,
+  deleteRoles,
+  FindUserPosts,
+  FindUsersActions,
+  getRequestFriends,
+};
