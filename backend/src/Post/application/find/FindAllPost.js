69cb2882d9def1ea663a0370676bea1693aa448e
JorgeLap <jorgais.ernesto@gmail.com>
2023-09-28 12:35:17 -0700

    fixed the findallpost control of data

--- backend/src/Post/application/find/FindAllPost.js
+++ backend/src/Post/application/find/FindAllPost.js
@@ -3,9 +3,13 @@ const Post = require("../../dominio/Post");
 const FindAllPost = async (req, res) => {
   const limit = parseInt(req.query.limit, 10) || 10;
   const page = parseInt(req.query.page, 10) || 1;
-  const posts = await Post.paginate({}, { limit, page });
 
-  return res.status(200).json(posts);
+  try {
+    const posts = await Post.paginate({}, { limit, page });
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(500).json({ message: "algo salio mal " });
+  }
 };
 
 module.exports = FindAllPost;
