b87d9580cb75f4d76ca21d133a201ed462b96c82
JorgeLap <jorgais.ernesto@gmail.com>
2023-09-29 14:54:39 -0700

    add request find post by id

--- backend/src/Post/application/find/FindPostById.js
+++ backend/src/Post/application/find/FindPostById.js
@@ -1,22 +1,15 @@
-const Post = require("../../dominio/Post")
+const Post = require("../../dominio/Post");
 
+const findPostById = async (req, res) => {
+  const { id } = req.params;
 
+  try {
+    const post = await Post.findById(id);
 
-const findPostById = async (req, res)=>{
+    return res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ message: "algo salio mal" });
+  }
+};
 
-      const {id} = req.params
-
-      try {
-              const post = await Post.findById(id).populate('votes').populate('comments').populate('feeling')
-              
-              return res.status(200).json(post)
-                  
-                 
-      } catch (error) {
-            
-            res.status(500).json({message:'algo salio mal'})
-      }
-      }
-
-
-module.exports  = findPostById
+module.exports = findPostById;
