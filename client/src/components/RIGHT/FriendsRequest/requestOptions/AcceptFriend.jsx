db9cda0e58de8bbec8759737565d6a5dc438240a
Jorge Ernesto <lower.joerge@gmail.com>
2023-09-28 23:03:18 -0700

    add two buttons accept and decilne to request frie

---
+++ client/src/components/RIGHT/FriendsRequest/requestOptions/AcceptFriend.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/no-unknown-property */
+import React, { useCallback } from 'react'
+import './AcceptFriend.css'
+import useMutationRequest from '../../../../hooks/useMutationRequest'
+import OptionsRequestFriends from './OptionsRequestFriends'
+import { useSelector } from 'react-redux'
+import toast, { Toaster } from 'react-hot-toast'
+import LoaderVote from '../../../MIDDLE/post/Votes/LoaderVote'
+
+const AcceptFriend = ({ userRequestFriend }) => {
+    const { _id: currentUser } = useSelector(
+        (state) => state.user.currentUser.user,
+    );
+
+    const { mutate, isLoadingMutation, isError } = useMutationRequest(OptionsRequestFriends, { name: 'requestFriends' })
+
+    const handleMutate = useCallback(() => {
+        mutate({
+            userRequestFriend,
+            userAccept: currentUser,
+            accept: true
+        }, {
+            onSuccess: () => {
+                toast.success('anadiste a un nuevo amigo', {
+                })
+            }
+        })
+    })
+
+    return (
+        <button className="Accept-friend-accept" onClick={() => handleMutate()}>
+            {isLoadingMutation && <LoaderVote />}
+            {!isLoadingMutation && <span>Aceptar</span>}
+
+        </button>
+    )
+}
+
+export default AcceptFriend
---
+++ client/src/components/RIGHT/FriendsRequest/requestOptions/DeclineFriend.jsx
@@ -0,0 +1,31 @@
+import React, { useCallback } from 'react'
+import './DeclineFriend.css'
+import useMutationRequest from '../../../../hooks/useMutationRequest';
+import { useSelector } from 'react-redux';
+import toast from 'react-hot-toast';
+import OptionsRequestFriends from './OptionsRequestFriends';
+import LoaderVote from '../../../MIDDLE/post/Votes/LoaderVote';
+const DeclineFriend = ({ userRequestFriend }) => {
+
+    const { _id: currentUser } = useSelector(
+        (state) => state.user.currentUser.user,
+    );
+
+    const { mutate, isLoadingMutation, isError } = useMutationRequest(OptionsRequestFriends, { name: 'requestFriends' })
+
+    const handleMutate = useCallback(() => {
+        mutate({
+            userRequestFriend,
+            userAccept: currentUser,
+            accept: true
+        })
+    })
+    return (
+        <button className="Accept-friend" onClick={() => handleMutate()}>
+            {isLoadingMutation && <LoaderVote />}
+            {!isLoadingMutation && <span>Decline</span>}
+        </button>
+    )
+}
+
+export default DeclineFriend
