dcd0fffa6ba452c1e6e99d2c4b484519da73b977
JorgeLap <jorgais.ernesto@gmail.com>
2023-09-29 14:57:12 -0700

    add diferent request options to ellipsi of post

---
+++ client/src/components/MIDDLE/post/more/ellipsiOwnUser/RequestOptionsOwnUser.jsx
@@ -0,0 +1,17 @@
+import useMutationRequest from "../../../../../hooks/useMutationRequest";
+import { DeletePostService, EditPostService } from "../../services/post/post.service";
+
+
+export const DeletePost = ()=>{
+  const {mutate, isLoadingMutation, isError, reset} = useMutationRequest(DeletePostService, {name:'posts'})
+
+  return {mutateDelete:mutate, isLoadingMutationDelete:isLoadingMutation, isErrorDeletePost:isError, resetDelete:reset}
+}
+
+export const  EditPostMutation = ()=>{
+
+    const {mutate, isLoadingMutation, isError} = useMutationRequest(EditPostService, {name:'posts'})
+
+    return {mutateEdit:mutate, isLoadingMutationEdit:isLoadingMutation, isErrorEditPost:isError}
+
+}
