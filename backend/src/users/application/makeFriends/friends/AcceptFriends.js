50d5c8708413568826c6b4bb44d8263310f63475
Jorge Ernesto <lower.joerge@gmail.com>
2023-09-28 23:01:33 -0700

    add accept/decline requestFriend

--- backend/src/users/application/makeFriends/friends/AcceptFriends.js
+++ backend/src/users/application/makeFriends/friends/AcceptFriends.js
@@ -5,32 +5,37 @@ const AcceptFriends = async (req, res)=>{
 
   //userWaiting es el user que esta esperando a que lo acepten
   //userAccept es el user que va aceptar
+  if(!userAccept) return res.status(500).json({message:"algo salio mal"})
 
   const userForAccept = await User.findById(userAccept)
 
-
-  if(!userForAccept.friendsWaiting.includes(userWaiting)){
-    return res.status(500).json({message:"no estas en la lista de espera"})
-
-  }
-
- try {
-
-  if(accept){
-
-    await userForAccept.updateOne({$pull:{friendsWaiting:userWaiting}})
-      await userForAccept.updateOne({$push:{friends:userWaiting}})
-      
-      return res.status(200).json({message:"acceptado"})
-  }
-
-   await userForAccept.updateOne({$pull:{friendsWaiting:userWaiting}})
-    return res.status(200).json({message:"se ha cancelado la solicitud"})
+  if(!userForAccept.friendsWaiting.includes(userWaiting)) return res.status(500).json({message:"no estas en la lista de espera"})
     
- } catch (error) {
-   
- }
+    try {
+
+      const acceptBoolean = (accept === 'true')
      
+
+
+    
+      if(acceptBoolean){
+        await userForAccept.updateOne({$pull:{friendsWaiting:userWaiting}})
+        await userForAccept.updateOne({$push:{friends:userWaiting}})
+        
+        return res.status(200).json({message:"acceptado"})
+      }
+    
+      if(!acceptBoolean){
+    
+        await userForAccept.updateOne({$pull:{friendsWaiting:userWaiting}})
+         return res.status(200).json({message:"se ha cancelado la solicitud"})
+      }
+    
+        
+     } catch (error) {
+      return res.status(500).json({message:"algo salio mal"})
+     }
+         
     
    
     
