58934f72409367417e2066ea73bb3ee200e1dbad
JorgeLap <jorgais.ernesto@gmail.com>
2023-09-28 18:09:08 -0700

    fixed navbar and component search

--- client/src/components/Navbar/componentNavbar/Navbar.jsx
+++ client/src/components/Navbar/componentNavbar/Navbar.jsx
@@ -1,37 +1,19 @@
 import "./Navbar.css";
 
 // librerias
-import React, { useState } from "react";
+import React  from "react";
 import rem from "../../../assets/rem.jpg";
-import { Users } from "../../RIGHT/SearchFriends/User";
+import Search from "../Search/Search";
 
-// icons
-
-import { BsSearch } from "react-icons/bs";
-import SearchingPeople from "../SearchingPeople";
 
 const Navbar = () => {
-  const [query, setquery] = useState("");
+
 
   return (
     <nav>
       <div className="container">
         <h2 className="log">cbta 81</h2>
-        <div className="search-bar">
-          <i>
-            <BsSearch />
-          </i>
-
-          <input
-            type="search"
-            placeholder="Search"
-            value={query}
-            onChange={(e) => setquery(e.target.value)}
-          />
-          <div className="searchingPeople">
-            <SearchingPeople Users={Users} query={query}></SearchingPeople>
-          </div>
-        </div>
+        <Search/>
         <div className="create">
           <div className="profile-photo">
             <img src={rem} alt="" />
