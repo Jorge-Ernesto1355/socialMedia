5b82756448d33f85754e81ff26a56f61123f2991
JorgeLap <jorgais.ernesto@gmail.com>
2023-09-28 18:11:33 -0700

    new request to get request of friends

--- backend/src/users/infrastructure/User.routes.js
+++ backend/src/users/infrastructure/User.routes.js
@@ -1,60 +1,54 @@
-const {Router} = require('express')
-const index = require('../application/index')
-const indexAuth = require('../application/authUser/indexAuth')
-const DeleteUser = require('../application/Delete/DeleteUserById')
-const UpdateUserProfile = require('../application/Update/UpdateUserProfile')
-const verifyToken = require('../../auth/application/verifySignup')
+const { Router } = require("express");
+const index = require("../application/index");
+const indexAuth = require("../application/authUser/indexAuth");
+const DeleteUser = require("../application/Delete/DeleteUserById");
+const UpdateUserProfile = require("../application/Update/UpdateUserProfile");
+const verifyToken = require("../../auth/application/verifySignup");
 
+const router = Router();
 
-const router = Router()
-
-
-//get All Users 
-router.get('/',  index.FindAllUsers)
+//get All Users
+router.get("/", index.FindAllUsers);
 
 //get one User
 
-router.get('/:id', index.FindUserById)
+router.get("/:id", index.FindUserById);
 
 //delete User
-router.delete('/:id',  DeleteUser )
+router.delete("/:id", DeleteUser);
 
-router.put('/password',indexAuth.NewPassword)
+router.put("/password", indexAuth.NewPassword);
 
-router.put('/friend/acceptfriend', index.AcceptFriends)
+router.put("/friend/acceptfriend", index.AcceptFriends);
 
-router.put('/friend/addfriend', index.addFriend)
+router.get("/friend/request/all/:userId", index.getRequestFriends);
+
+router.put("/friend/add", index.addFriend);
 
 //update User
-router.put('/:id', UpdateUserProfile)
+router.put("/:id", UpdateUserProfile);
 
 //uploadImages
 
-router.post('/upload', index.UploadProfilePicture)
+router.post("/upload", index.UploadProfilePicture);
 
-router.post('/upload/cover', index.UploadCover)
-
+router.post("/upload/cover", index.UploadCover);
 
 //relationship
 
+router.put("/relationShip/add", index.AcceptRelationShip);
 
-router.put('/relationShip/add', index.AcceptRelationShip)
+router.post("/relationShip", index.addRelationShip);
 
-router.post('/relationShip', index.addRelationShip)
-
-router.delete('/relationShip/del', index.deleteRelationShip)
+router.delete("/relationShip/del", index.deleteRelationShip);
 
 //roles
-router.put('/roles/add', index.giveRoles )
+router.put("/roles/add", index.giveRoles);
 
-router.delete('/roles/del', index.deleteRoles)
+router.delete("/roles/del", index.deleteRoles);
 
-router.get('/userPosts/:userId', index.FindUserPosts)
+router.get("/userPosts/:userId", index.FindUserPosts);
 
-router.get('/actions/getUsers/:id', index.FindUsersActions)
+router.get("/actions/getUsers/:id", index.FindUsersActions);
 
-
-
-
-
-module.exports = router
+module.exports = router;
