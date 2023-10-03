883b84aa4f8009799ed1c451117b23df0b0c18b4
JorgeLap <jorgais.ernesto@gmail.com>
2023-10-03 14:36:15 -0700

    add implementation add friend

--- client/src/components/Navbar/Search/user/User.jsx
+++ client/src/components/Navbar/Search/user/User.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import './User.css'
 import rem from '../../../../assets/rem.jpg'
 import rightArrow from '../icons/rightArrow.png'
+import useMutationRequest from '../../../../hooks/useMutationRequest'
+import Friend from './Friend'
 const User = ({hit, itemsProps}) => {
+
+  
   return (
     <li className='search-user-container' {...itemsProps}>
        
         <img className='search-user-img' src={hit?.imageProfile?.url ?? rem} alt="" />
        <div className='user-information-container'>
        <h5 className='user-username'>{hit?.username}</h5>
-       <p className='user-status'>friend</p>
+       <Friend addUser={hit}/>
        </div>
-       
-        
     </li>
   )
 }
