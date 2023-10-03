a3ba85712fd2569337ad778177fe200bd07b68e6
JorgeLap <jorgais.ernesto@gmail.com>
2023-10-03 14:34:38 -0700

    add some bugs of add friend

--- backend/src/Post/dominio/Reaction.js
+++ backend/src/Post/dominio/Reaction.js
@@ -1,20 +1,23 @@
-const {Schema, model} = require('mongoose')
-const mongoosePaginate = require('mongoose-paginate-v2')
+const { Schema, model } = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
 
-const Reaction = Schema({
-    label:{type:String},
-    user:{
-     userId:Schema.Types.ObjectId, 
-     username:String,
-     imageProfile:{
-       url:String,
-       public_id:String
-     },
-    }
- },{
-    strictPopulate:false,
-     versionKey: false,
- })
+const Reaction = Schema(
+  {
+    label: { type: String },
+    user: {
+      userId: Schema.Types.ObjectId,
+      username: String,
+      imageProfile: {
+        url: String,
+        public_id: String,
+      },
+    },
+  },
+  {
+    strictPopulate: false,
+    versionKey: false,
+  }
+);
 
-Reaction.plugin(mongoosePaginate)
-module.exports = model('Reaction', Reaction)
+Reaction.plugin(mongoosePaginate);
+module.exports = model("Reaction", Reaction);
