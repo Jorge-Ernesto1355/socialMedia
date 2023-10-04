0b04228403e34516d221a0a82de907c8a074709b
Jorge Ernesto <lower.joerge@gmail.com>
2023-10-03 19:38:48 -0700

    added PostService

--- backend/src/Post/PostService.js
+++ backend/src/Post/PostService.js
@@ -1,23 +1,57 @@
+const UserModel = require("../users/domain/UserModel");
 const createImagen = require("./application/createPost/createImagen");
 const createVotes = require("./application/createPost/createVotes");
-
+const Post = require('./dominio/Post')
 function exits(object) {
-  if (!objectCreate) throw new Error("not found parameters");
+  if (!object) throw new Error("not found parameters");
 }
 
-  static async difusion(object) {
-    exits(object);
+class PostService {
 
-    const { description, userId, votes, difusion, postShared, usersTagged } =
-      object;
+  static async getAll(object){
 
+    exits(object)
+
+    const {limit, page} = object
+
+    try {
+      return await Post.paginate({}, { limit, page });
+    } catch (error) {
+      return {
+        error,
+        message: error.message,
+      };
+    }
+
+  }
+
+  static async create(req) {
+    exits(req.body);
+  
+    const { description, userId, votes, postShared, usersTagged } = req.body
+  
     let image = null;
     try {
       image = await createImagen(req);
-      votes > 0 && (await createVotes({ req, res, votes }));
-
+      votes > 0 && (await createVotes({votes}));
+      const user = await UserModel.findById(userId)
+  
       const newPost = new Post({ userId, description, image });
       if (postShared) newPost.postShared = postShared;
-    } catch (error) {}
+      user.posts = [...user.posts, newPost]
+      await user.save();
+      const postsaved = await newPost.save();
+      return postsaved
+
+    } catch (error) {
+      return {
+        error,
+        message: error.message,
+      };
+    }
   }
+  
 }
+
+module.exports = PostService
+
