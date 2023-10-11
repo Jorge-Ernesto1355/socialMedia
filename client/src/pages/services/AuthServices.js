030bbce697352ea3eaa6503d7aa6608b3b6e96eb
JorgeLap <jorgais.ernesto@gmail.com>
2023-10-11 14:16:47 -0700

    fixed some bugs

--- client/src/pages/services/AuthServices.js
+++ client/src/pages/services/AuthServices.js
@@ -1,53 +1,50 @@
-import axios from "axios"
+import axios from "axios";
 
-const { publicRequest, BASE_URL } = require("../../utilities/requestMethod")
-
+const { publicRequest, BASE_URL } = require("../../utilities/requestMethod");
 
 class AuthService {
-    static async Register(user){
-        try {
-            
-            return publicRequest.post('/auth/register', user)
-        } catch (error) {
-             return error
-        }
+  static async Register(user) {
+    try {
+      return publicRequest.post("/auth/register", user);
+    } catch (error) {
+      return error;
     }
+  }
 
-    static async Login(user){
-        try {
-            return publicRequest.post('/auth/login', user)
-        } catch (error) {
-            return error
-        }
+  static async Login(user) {
+    try {
+      return publicRequest.post("/auth/login", user);
+    } catch (error) {
+      return error;
     }
+  }
 
-    static async refreshToken(refresh){
-
-        const config = {
-            headers:{
-              "Authorization": `Bearer ${refresh}`,
-            }
-          };
-        try {
-            return axios.put(BASE_URL+'/auth/refresh', {}, config)
-        } catch (error) {
-            return error
-        }
+  static async refreshToken(refresh) {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${refresh}`,
+      },
+    };
+    try {
+      return axios.put(BASE_URL + "/auth/refresh", {}, config);
+    } catch (error) {
+      return error;
     }
+  }
 
-    static async logOut(refresh){
-        const config = {
-            headers:{
-              "Authorization": `Bearer ${refresh}`,
-            }
-          };
+  static async logOut(refresh) {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${refresh}`,
+      },
+    };
 
-        try {
-            return axios.put(BASE_URL + '/auth/logout', {}, config)
-        } catch (error) {
-            return error
-        }
+    try {
+      return axios.put(BASE_URL + "/auth/logout", {}, config);
+    } catch (error) {
+      return error;
     }
+  }
 }
 
-export default AuthService
+export default AuthService;
