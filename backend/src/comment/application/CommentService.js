e65d2a9837ddec491f285d44335f6db2edda6248
JorgeLap <jorgais.ernesto@gmail.com>
2023-10-11 14:08:14 -0700

    fixed some bugs

--- backend/src/comment/application/CommentService.js
+++ backend/src/comment/application/CommentService.js
@@ -1,38 +1,33 @@
-
-
-const validateObjectId = require('../../libs/isValidObjectId')
-const exits = require('../../libs/exits')
+const validateObjectId = require("../../libs/isValidObjectId");
+const exits = require("../../libs/exits");
 const Comment = require("../domain/comments");
-const createImagen = require('../../Post/application/createPost/createImagen');
-const { default: mongoose } = require('mongoose');
+const createImagen = require("../../Post/application/createPost/createImagen");
+const { default: mongoose } = require("mongoose");
 
 class CommentService {
   static async getAll(object) {
     try {
       exits(object);
-  
-      const { containerId, type, limit, page} = object;
-  
+
+      const { containerId, type, limit, page } = object;
+
       const options = {
         limit,
         page,
-       
       };
-      const validateContainer = await validateObjectId(containerId, type);
+      const container = await validateObjectId({ _id: containerId }, type);
 
-      if (validateContainer?.error) {
+      if (container?.error) {
         throw new Error(validateContainer?.error?.message);
       }
 
-      const container = await mongoose.models[type].findById(containerId)
-    
+      const commentsIds = container.comments.map((commentId) => commentId);
 
-    const commentsIds = container.comments.map((commentId)=> commentId)
-      
-
-      const comments = await Comment.paginate({_id:{$in:commentsIds}}, options);
-      return comments
-
+      const comments = await Comment.paginate(
+        { _id: { $in: commentsIds } },
+        options
+      );
+      return comments;
     } catch (error) {
       return {
         error,
@@ -41,46 +36,48 @@ class CommentService {
     }
   }
 
-  static async getResponded(object){
+  static async getResponded(object) {
     try {
-      exits(object)
-      
-      const {commentId ,limit, page} = object
+      exits(object);
+
+      const { commentId, limit, page } = object;
       const validateComment = await validateObjectId(commentId, "Comment");
 
-    if (validateComment?.error) {
-      throw new Error(validateComment?.error?.message);
-    }
+      if (validateComment?.error) {
+        throw new Error(validateComment?.error?.message);
+      }
 
-    const comment = await Comment.findById(commentId)
+      const comment = await Comment.findById(commentId);
 
-    const commentsResponded = await Comment.paginate({_id:{$in: comment.commentsResponded?.map((commentId)=> commentId)}}, {page, limit})
- 
-    return commentsResponded
+      const commentsResponded = await Comment.paginate(
+        {
+          _id: {
+            $in: comment.commentsResponded?.map((commentId) => commentId),
+          },
+        },
+        { page, limit }
+      );
 
+      return commentsResponded;
     } catch (error) {
       return {
-        error, 
-        message:error.message
-      }
+        error,
+        message: error.message,
+      };
     }
-
-       
   }
 
   static async create(object) {
     try {
       exits(object);
-      const {containerId, userId, text, type, commentId} = object
-      const isValidUser = await validateObjectId(userId, "User");
-      const isValidContainer = await validateObjectId(containerId, type);
+      const { containerId, userId, text, type, commentId } = object;
+      const user = await validateObjectId({ _id: userId }, "User");
+      const container = await validateObjectId({ _id: containerId }, type);
 
-      if (isValidContainer?.error || isValidUser?.error) {
+      if (container?.error || user?.error) {
         throw new Error("document not found or objectId is not valid");
       }
 
-      const container =  await mongoose.models[type].findById(containerId)
-     
       const image = await createImagen(object.files);
       const comment = await new Comment({
         comment: { userId, text, image, containerId },
@@ -96,9 +93,10 @@ class CommentService {
         return await commentGot.save();
       }
 
-      if(container.comments) container.comments = [...container.comments, commentCreated]
-      
-      await container.save()
+      if (container.comments)
+        container.comments = [...container.comments, commentCreated];
+
+      await container.save();
       return commentCreated;
     } catch (error) {
       return {
@@ -108,44 +106,18 @@ class CommentService {
     }
   }
 
-  static async delete(object){
+  static async delete(object) {
     try {
-      exits(object)
-      
-      const {commentId} = object
+      exits(object);
+
+      const { commentId } = object;
       const validateComment = await validateObjectId(commentId, "Comment");
 
-    if (validateComment?.error) {
-      throw new Error(validateComment?.error?.message);
-    }
+      if (validateComment?.error) {
+        throw new Error(validateComment?.error?.message);
+      }
 
-    await Comment.findByIdAndDelete(commentId)
-
-  }
-  catch(error){
-   return {
-        error,
-        message: error.message,
-      };
-    }
-  }
-
-  static async update(object){
-    try {
-      exits(object)
-      
-      const {commentId, text} = object
-      const validateComment = await validateObjectId(commentId, "Comment");
-
-    if (validateComment?.error) {
-      throw new Error(validateContainer?.error?.message);
-    }
-    const comment = await Comment.findById(commentId)
-    
-    comment.comment.text = text
-    comment.edit = true
-    await comment.save()
-    return comment
+      await Comment.findByIdAndDelete(commentId);
     } catch (error) {
       return {
         error,
@@ -154,9 +126,29 @@ class CommentService {
     }
   }
 
-  
-  
+  static async update(object) {
+    try {
+      exits(object);
 
+      const { commentId, text } = object;
+      const validateComment = await validateObjectId(commentId, "Comment");
+
+      if (validateComment?.error) {
+        throw new Error(validateContainer?.error?.message);
+      }
+      const comment = await Comment.findById(commentId);
+
+      comment.comment.text = text;
+      comment.edit = true;
+      await comment.save();
+      return comment;
+    } catch (error) {
+      return {
+        error,
+        message: error.message,
+      };
+    }
+  }
 }
 
 module.exports = CommentService;
