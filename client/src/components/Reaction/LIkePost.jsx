2245a70065bd0f7ded44bf4e8dd65edc8124546a
JorgeLap <jorgais.ernesto@gmail.com>
2023-10-11 14:19:47 -0700

    fixed some bugs with importations

---
+++ client/src/components/Reaction/LIkePost.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./likePost.css";
+import { motion } from "framer-motion";
+import like from "../../assets/likePost.png";
+import { objetsImgs } from "../MIDDLE/post/post/objectImg";
+
+
+const LikePost = ({ reactionType, setShowReactions }) => {
+  return (
+    <div className="ratingIcon">
+      {reactionType === null ? (
+        <div
+          className="reaction-div"
+          onClick={() => setShowReactions((prev) => !prev)}
+          onMouseEnter={() => setShowReactions(true)}
+        >
+          <img className="icon" src={like} alt="like-post" />
+          <span className="reaction-text">me gusta</span>
+        </div>
+      ) : (
+        <div className="actionsActivated">
+          <motion.img
+            initial={{ scale: 0 }}
+            animate={{ scale: 1.1 }}
+            src={objetsImgs[reactionType]}
+            alt="reactiontype-likeSelected"
+          />
+          <span
+            className={reactionType}
+            onClick={() => setShowReactions((prev) => !prev)}
+          >
+            me {reactionType}
+          </span>
+        </div>
+      )
+      }
+    </div >
+  )
+}
+
+export default LikePost;
