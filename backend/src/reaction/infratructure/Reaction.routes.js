24053a7148ba5d07771cda7f42882ce46417ca96
JorgeLap <jorgais.ernesto@gmail.com>
2023-10-11 14:11:28 -0700

    added new route to get currents reactions

--- backend/src/reaction/infratructure/Reaction.routes.js
+++ backend/src/reaction/infratructure/Reaction.routes.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
-const index  = require('../index')
+const index = require("../index");
 
-const router = Router()
+const router = Router();
 
-router.put('/:containerId', index.mutation.React)
+router.put("/:containerId", index.mutation.React);
 
-router.get('/all/:containerId/:type',index.query.getReactions )
+router.get("/all/:containerId/:type", index.query.getReactions);
 
-router.get('/:containerId/:type', index.query.getReaction)
+router.get("/:containerId/:type", index.query.getReaction);
 
-module.exports = router
+router.get("/view/:containerId/:type", index.query.getViewReactions);
+
+module.exports = router;
