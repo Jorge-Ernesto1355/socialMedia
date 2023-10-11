4ec04caa6e32c5b9cf2cc092fc66a1cce305cc60
JorgeLap <jorgais.ernesto@gmail.com>
2023-10-11 14:08:02 -0700

    deleted unecessary keys of comment shema

--- backend/src/auth/application/verifySignup.js
+++ backend/src/auth/application/verifySignup.js
@@ -1,34 +1,28 @@
-const jwt = require("jsonwebtoken")
-const User = require('../../users/domain/UserModel')
-const SECRET = require('../../dotenv');
+const jwt = require("jsonwebtoken");
+const User = require("../../users/domain/UserModel");
+const SECRET = require("../../dotenv");
 
+const verifyToken = async (req, res, next) => {
+  var token = req?.headers?.authorization?.split(" ")[1];
 
-const verifyToken = async (req, res, next)=>{
-  var token = req?.headers?.authorization?.split(' ')[1];
-  console.log(token)
+  if (!token)
+    return res.status(403).json({
+      message: "no token provider or invalid token ",
+    });
 
-  if(!token) return res.status(403).json({
-    message:"no token provider or invalid token "
-  })
-
-  if(token){
-    
+  if (token) {
     try {
-   
-     const decoded = jwt.verify(token, SECRET.ACCESS_TOKEN_SECRET)
-     if(decoded) {
-       req.userId = decoded.id
-       next()
-     }else{
-      return res.status(403).json({error:'not autorizado'})
-     }
-   
-    
+      const decoded = jwt.verify(token, SECRET.ACCESS_TOKEN_SECRET);
+      if (decoded) {
+        req.userId = decoded.id;
+        next();
+      } else {
+        return res.status(403).json({ error: "not autorizado" });
+      }
     } catch (error) {
-       return res.status(403).json({error:error.message})
+      return res.status(403).json({ error: error.message });
     }
   }
+};
 
-}
-
-module.exports = verifyToken
+module.exports = verifyToken;
