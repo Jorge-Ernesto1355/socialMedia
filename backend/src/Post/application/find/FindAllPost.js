f68fddffc4f1c14fffae8c36ec41114df7224e3c
JorgeLap <jorgais.ernesto@gmail.com>
2023-10-11 14:09:15 -0700

    fixed error message of reaction schema

--- backend/src/Post/application/find/FindAllPost.js
+++ backend/src/Post/application/find/FindAllPost.js
@@ -5,17 +5,13 @@ const FindAllPost = async (req, res) => {
   const limit = parseInt(req.query.limit, 10) || 10;
   const page = parseInt(req.query.page, 10) || 1;
 
-  console.log('siisisi')
+  const posts = await PostService.getAll({ limit, page });
 
-  
-    const posts = await PostService.getAll({limit, page})
+  if (posts.error) {
+    return res.status(400).json({ message: posts.error.message });
+  }
 
-    if(posts.error){
-      return res.status(400).json({ message: posts.error.message });
-    }
-    
-    return res.status(200).json(posts);
-  
+  return res.status(200).json(posts);
 };
 
 module.exports = FindAllPost;
--- backend/src/reaction/index.js
+++ backend/src/reaction/index.js
@@ -1,17 +1,16 @@
-const React = require('./application/React')
+const React = require("./application/React");
 
-
-
-const getReactions = require('./application/getReactions')
-const getReaction = require('./application/getReaction')
+const getReactions = require("./application/getReactions");
+const getReaction = require("./application/getReaction");
+const getViewReactions = require("./application/reactionsView");
 
 module.exports = {
-  mutation:{
-    React
-  }, 
-  query:{
-    getReactions, 
-    getReaction
-  }
-    
-}
+  mutation: {
+    React,
+  },
+  query: {
+    getReactions,
+    getReaction,
+    getViewReactions,
+  },
+};
