47645213cb5d4df68387ef9bbb107ac1508beae4
JorgeLap <jorgais.ernesto@gmail.com>
2023-10-11 14:07:23 -0700

    fixed login user error message

--- backend/src/auth/application/AuthService.js
+++ backend/src/auth/application/AuthService.js
@@ -1,112 +1,111 @@
+const { encryptPassword, comparePassword } = require("./auth");
+const exits = require("../../libs/exits");
+const isValidObjectId = require("../../libs/isValidObjectId");
+const UserModel = require("../../users/domain/UserModel");
+const { REFRESH_TOKEN_SECRET } = require("../../dotenv");
+const jwt = require("jsonwebtoken");
+const TokenModel = require("../../users/domain/TokenModel");
 
-const { encryptPassword, comparePassword } = require("./auth")
-const exits = require("../../libs/exits")
-const isValidObjectId = require("../../libs/isValidObjectId")
-const UserModel = require("../../users/domain/UserModel")
-const { REFRESH_TOKEN_SECRET } = require("../../dotenv")
-const jwt = require('jsonwebtoken')
-const TokenModel = require("../../users/domain/TokenModel")
-
-module.exports = class AuthService{
-    static async Register(object){
-        try {
-            exits(object)
-            const {username, email, password} = object
-
-            const usernameValid = await isValidObjectId({username}, "User")
-            const emailValid = await isValidObjectId({email}, "User")
-            
-            if(!usernameValid.error) throw new Error('username is already taken')
-
-            if(!emailValid.error) throw new Error('email is aleady taken')
-
-            const passwordEncryped = await encryptPassword(password)
-            
-            const user = new UserModel({ username, email, password:passwordEncryped})
-
-            await user.save()
-
-            const accessToken = user.createAccessToken()
-
-            const refreshToken = await  user.createRefreshToken()
-
-            return {user, accessToken, refreshToken}
-            
-        } catch (error) {
-            return {
-                error, 
-                message:error.message
-            }
-        }
-    }
-
-    static async login(object){
-        try {
-            
-            exits(object)
-            const {email, password} = object
-            const emailExits = await isValidObjectId({email}, "User")
-
-            if(emailExits.error)  throw new Error('email or password are not corrects')
-
-            const correctPassword = await comparePassword(password, emailExits.password)
-           
-            if(!correctPassword) throw new Error('email or password are not corrects')
-
-            const accessToken = emailExits.createAccessToken()
-
-            const refreshToken = await emailExits.createRefreshToken()
-
-            return {user:emailExits, accessToken, refreshToken}
-           
-        } catch (error) {
-            return {
-                error, 
-                message:error.message
-            }
-        }
-    }
-
-   static async refresh(object){
+module.exports = class AuthService {
+  static async Register(object) {
     try {
+      exits(object);
+      const { username, email, password } = object;
 
-        exits(object) 
-        const {refreshToken} = object
+      const usernameValid = await isValidObjectId({ username }, "User");
+      const emailValid = await isValidObjectId({ email }, "User");
 
-       
-        const verifyRefresh = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET)
-        const {id} = verifyRefresh
+      if (!usernameValid.error) throw new Error("username is already taken");
 
-        const user = await isValidObjectId({userId:id}, "User")
-        const accessToken = user.createAccessToken()
+      if (!emailValid.error) throw new Error("email is aleady taken");
 
-        return accessToken
-      } catch (error) {
-        return {
-            error, 
-            message: error.message
-        }
-      }
+      const passwordEncryped = await encryptPassword(password);
 
-   }
+      const user = new UserModel({
+        username,
+        email,
+        password: passwordEncryped,
+      });
 
-   static async logOut(object){
-    try {
-        exits(object)
+      await user.save();
 
-        const {refreshToken} = object
+      const accessToken = user.createAccessToken();
 
-        const verifyRefresh = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET)
-        const {id} = verifyRefresh
+      const refreshToken = await user.createRefreshToken();
 
-        const user = await isValidObjectId({userId:id}, "User")
-
-        user.refreshToken = ''
-        await user.save()
-        return 
-        
+      return { user, accessToken, refreshToken };
     } catch (error) {
-        
+      return {
+        error,
+        message: error.message,
+      };
     }
-   }
-}
+  }
+
+  static async login(object) {
+    try {
+      exits(object);
+      const { email, password } = object;
+      const emailExits = await isValidObjectId({ email }, "User");
+
+      if (emailExits.error)
+        throw new Error("email or password are not corrects");
+
+      const correctPassword = await comparePassword(
+        password,
+        emailExits.password
+      );
+
+      if (!correctPassword)
+        throw new Error("email or password are not corrects");
+
+      const accessToken = emailExits.createAccessToken();
+
+      const refreshToken = await emailExits.createRefreshToken();
+
+      return { user: emailExits, accessToken, refreshToken };
+    } catch (error) {
+      return {
+        error,
+        message: error.message,
+      };
+    }
+  }
+
+  static async refresh(object) {
+    try {
+      exits(object);
+      const { refreshToken } = object;
+
+      const verifyRefresh = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET);
+      const { id } = verifyRefresh;
+
+      const user = await isValidObjectId({ userId: id }, "User");
+      const accessToken = user.createAccessToken();
+
+      return accessToken;
+    } catch (error) {
+      return {
+        error,
+        message: error.message,
+      };
+    }
+  }
+
+  static async logOut(object) {
+    try {
+      exits(object);
+
+      const { refreshToken } = object;
+
+      const verifyRefresh = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET);
+      const { id } = verifyRefresh;
+
+      const user = await isValidObjectId({ userId: id }, "User");
+
+      user.refreshToken = "";
+      await user.save();
+      return;
+    } catch (error) {}
+  }
+};
--- backend/src/auth/application/LoginUser.js
+++ backend/src/auth/application/LoginUser.js
@@ -1,28 +1,20 @@
+const { validateUserLogin } = require("../../users/validation/userSchema");
+const AuthService = require("./AuthService");
 
+const LoginUser = async (req, res) => {
+  const { email, password } = req.body;
 
-const { validateUserLogin } = require('../../users/validation/userSchema')
-const AuthService = require('./AuthService')
+  const result = validateUserLogin({ email, password });
 
-
-
-
-const LoginUser = async (req, res)=>{
-
-
-  const {email, password} = req.body
-
-  const result = validateUserLogin({email, password})
-
-  if(result.error){
-    return res.status(400).json({error:result.error.message})
+  if (result.error) {
+    return res.status(400).json({ error: result.error.message });
   }
 
-  const user = await AuthService.login({email, password})
+  const user = await AuthService.login({ email, password });
 
-  if(user?.error)  
-     return res.status(500).json({error: user.message})
+  if (user?.error) return res.status(500).json({ error: user.message });
 
-  return res.status(200).json(user)
-}
+  return res.status(200).json(user);
+};
 
-module.exports = LoginUser
+module.exports = LoginUser;
