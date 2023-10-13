0095c7949ddc6313dc5edea996a2755f6bd09da6
JorgeLap <jorgais.ernesto@gmail.com>
2023-10-13 15:21:10 -0700

    new cloudinary Services

--- backend/src/libs/cloudynary.js
+++ backend/src/libs/cloudynary.js
@@ -1,25 +1,44 @@
-const {v2} = require('cloudinary')
+const {v2: cloudinary} = require('cloudinary')
+const exits = require('./exits')
 
-v2.config({
+cloudinary.config({sd'fsd'fspkgeoiqytg
   cloud_name:"cbta", 
   api_key:"911678697972225", 
   api_secret:"k5TzOoP0uMgprtv1sDAprFTwtHo"
 
 })
 
-const uploadImage = async  (filePath)=>{
- return await  v2.uploader.upload(filePath, {
-    folder:"prueba"
-  })
+
+class cloudinaryService {
+  static async upload(object){
+    try {
+      exits(object)
+      const {filePath} = object
+      return await cloudinary.uploader.upload(filePath, {
+        folder:"prueba"
+      })
+    } catch (error) {
+      return {
+        error, 
+        message: error.message
+      }
+    }
+  }
+
+  static async deleteImage(object){
+    try {
+      exits(object)
+      const {imageId} = object 
+      await v2.uploader.destroy(imageId)
+    } catch (error) {
+      return {
+        error, 
+        message: error.message
+      }
+    }
+  }
 }
 
-const deleteImage = async (id)=>{
-   await v2.uploader.destroy(id)
-}
-
-module.exports = {
-  deleteImage, 
-  uploadImage
-}
+module.exports = cloudinaryService
 
 
