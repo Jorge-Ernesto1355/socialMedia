a7d102810d6a049b48769dcaa1170142f90b06fc
Jorge Ernesto <lower.joerge@gmail.com>
2023-10-13 14:46:38 -0700

    implementation postService.delete

--- backend/src/Post/PostService.js
+++ backend/src/Post/PostService.js
@@ -1,4 +1,7 @@
-const UserModel = require("../users/domain/UserModel");
+
+const isValidObjectId = require("../libs/isValidObjectId");
+const getTotalPoints = require("../reaction/utils/getTotalPoints");
+const userService = require("../users/userService");
 const createImagen = require("./application/createPost/createImagen");
 const createVotes = require("./application/createPost/createVotes");
 const Post = require("./dominio/Post");
@@ -21,6 +24,8 @@ class PostService {
       };
     }
   }
+
+
 
   static async get(object) {
     try {
@@ -45,7 +50,51 @@ class PostService {
   static async getTimeLine(object) {
     try {
       exits(object);
-      const { objectId } = object;
+      const { userId, limit, page} = object;
+
+      const userPosts = await userService.getPosts({userId, limit, page})
+
+      if(userPosts?.error){
+   
+        throw new Error(userPosts.message);
+      }
+
+      const friendPosts = await userService.getFriendsPosts({userId, limit, page})
+      if(friendPosts?.error){
+        
+        throw new Error(friendPosts?.message);
+      }
+
+      const options = {
+        limit, page
+      }
+
+      const currentUserAndFriendsPosts = Array.prototype.concat.apply(userPosts?.docs, friendPosts?.docs) ?? []
+
+      const idsPosts = currentUserAndFriendsPosts.map((currentPost)=> currentPost?._id)
+   
+      const results = await Post.paginate({_id: {$in: idsPosts}}, options)
+
+      const postsWithTotalPoints = await Promise.all(results?.docs?.map(async (currentPost) => {
+        const totalPoints = await getTotalPoints(currentPost?.reactions);
+      
+        if (totalPoints?.error) {
+          throw new Error(totalPoints?.message);
+        }
+      
+        if (typeof totalPoints === 'number') {
+          currentPost.points = totalPoints;
+        }
+      
+        return currentPost;
+      }));
+
+      const orderedPosts =  postsWithTotalPoints.sort((a, b) =>   b?.points - a?.points)
+
+      results.docs = orderedPosts
+
+      return results
+
     } catch (error) {
       return {
         error,
@@ -58,12 +107,16 @@ class PostService {
     exits(req.body);
 
     const { description, userId, votes, postShared, usersTagged } = req.body;
+    const queryOptions = {
+      model:'User', 
+      select:['posts']
+    }
 
     let image = null;
     try {
       image = await createImagen(req);
       votes > 0 && (await createVotes({ votes }));
-      const user = await UserModel.findById(userId);
+      const user = await isValidObjectId({_id:userId}, queryOptions)
 
       const newPost = new Post({ userId, description, image });
       if (postShared) newPost.postShared = postShared;
@@ -83,7 +136,11 @@ class PostService {
     try {
       exits(object);
       const { postId, text } = object;
-      const isValidPost = await validateObjectId(postId, "Post");
+      const queryOptions = {
+        model:'Post', 
+        
+      }
+      const isValidPost = await isValidObjectId({_id:postId}, queryOptions);
 
       if (isValidPost?.error) {
         throw new Error(isValidPost?.error?.message);
@@ -102,15 +159,40 @@ class PostService {
   static async delete(object) {
     try {
       exits(object);
-
       const { postId } = object;
-      const isValidPost = await validateObjectId(postId, "Post");
+      const queryOptions = {
+        model:'Post', 
+      }
+
+      const isValidPost = await isValidObjectId({_id:postId}, queryOptions);
+    
 
       if (isValidPost?.error) {
         throw new Error(isValidPost?.error?.message);
       }
 
       await Post.findByIdAndDelete(postId);
+
+      return null
+
+      // if(userShared){
+
+      //   try {
+      //     if(!post.shares.includes(userShared)){
+      //       return res.status(200).json({message:"se ha borrado"})
+      //     }else{
+  
+          
+      //       await post.UpdateOne({$pull:{shares: userShared}})
+      //       await user.UpdateOne({$pull:{posts:post}})
+    
+      //       await post.save()
+      //       await user.save()
+      //     }
+      //   } catch (error) {
+      //      return res.status(500).json({message:"recursos no coinciden"})
+      //   }
+      // }
     } catch (error) {
       return {
         error,
--- backend/src/Post/application/DeletePost.js
+++ backend/src/Post/application/DeletePost.js
@@ -1,64 +1,25 @@
-const Post = require("../dominio/Post")
 
-const User = require('../../users/domain/UserModel')
-const {deleteImage} = require('../../libs/cloudynary')
+const PostService = require("../PostService")
 
 const DeletePost = async (req, res)=>{
 
   
     const {id} = req.params
-    const {userShared, userId} = req.query
+    const {userId} = req.query
+    
 
-    //userShared es para borrar una publicacion que tu compartise 
-    //userId es para borrar una publicacion que tu creaste 
 
-    const post = await Post.findById(id)
-    const user = await User.findById(userId)
- 
-    if(userShared){
+    const deletePost = await PostService.delete({postId:id})
+    console.log(deletePost)
 
-      try {
-        if(!post.shares.includes(userShared)){
-          return res.status(200).json({message:"se ha borrado"})
-        }else{
-
-        
-          await post.UpdateOne({$pull:{shares: userShared}})
-          await user.UpdateOne({$pull:{posts:post}})
-  
-          await post.save()
-          await user.save()
-        }
-      } catch (error) {
-         return res.status(500).json({message:"recursos no coinciden"})
-      }
-    }
-
-    try {
-
-       if(userId === post.userId){
-         const post = await Post.findByIdAndDelete(id)
-
-         if(post && post.image.public_id){
-          
-          await deleteImage(post.image.public_id)
-       }
-
-      return  res.status(204).json({message:"recurso borrado"})
+    if(deletePost?.error){
       
-       
-
-     }
-     return res.status(500).json({message:'no puedes borrar un post que no es tuyo'})
-     
-    } catch (error) {
-       return res.status(500).json({message:"recursos no coinciden borrar completamente"})
+      return res.status(500).json({error:deletePost?.message})
     }
-    
-   
 
-    
-     
+    return res.status(204).json({message:'completed'})
+
+
     
 }
 
