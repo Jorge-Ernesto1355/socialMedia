afddf5e9655630e04b7851948ced4db1bae3a6a2
Jorge Ernesto <lower.joerge@gmail.com>
2023-10-13 15:00:18 -0700

    changed the request to services of post

--- client/src/components/MIDDLE/post/more/ellipsiOwnUser/RequestOptionsOwnUser.jsx
+++ client/src/components/MIDDLE/post/more/ellipsiOwnUser/RequestOptionsOwnUser.jsx
@@ -1,16 +1,17 @@
 import useMutationRequest from "../../../../../hooks/useMutationRequest";
-import { DeletePostService, EditPostService } from "../../services/post/post.service";
+import PostServices from "../../services/PostServices";
+import { EditPostService } from "../../services/post/post.service";
 
 
 export const DeletePost = () => {
-  const { mutate, isLoadingMutation, isError, reset } = useMutationRequest(DeletePostService, { name: 'posts' })
+  const { mutate, isLoadingMutation, isError, reset } = useMutationRequest(PostServices.delete, { name: 'posts' })
 
   return { mutateDelete: mutate, isLoadingMutationDelete: isLoadingMutation, isErrorDeletePost: isError, resetDelete: reset }
 }
 
 export const EditPostMutation = () => {
 
-  const { mutate, isLoadingMutation, isError, reset } = useMutationRequest(EditPostService, { name: 'posts' })
+  const { mutate, isLoadingMutation, isError, reset } = useMutationRequest(PostServices.update, { name: 'posts' })
 
   return { mutateEdit: mutate, isLoadingMutationEdit: isLoadingMutation, isErrorEditPost: isError, reset }
 
