765f226059a1fb4f31ccd85144ccd68232068f94
Jorge Ernesto <lower.joerge@gmail.com>
2023-10-13 15:04:56 -0700

    added new implementation of friend

--- client/src/components/Navbar/Search/user/Friend.jsx
+++ client/src/components/Navbar/Search/user/Friend.jsx
@@ -1,54 +1,53 @@
 import React, { useCallback } from 'react'
 import useMutationRequest from '../../../../hooks/useMutationRequest'
 import { addFriend } from '../service/users'
-import { useSelector } from 'react-redux'
-import { useCallbackRequest } from '../../../../hooks/useCallbackRequest/useCallbackRequest'
-import GetUser from '../../../../services/GetUser.service'
 import { isYourFriend } from '../../../../utilities/isYourFriend'
-import Loader from '../../../../utilities/Loader'
 import SpinnerLoader from '../../../../stylesComponents/spinnerLoader/SpinnerLoader'
+import AuthProvider from '../../../../zustand/AuthProvider'
+import userService from '../../../../services/UserService'
+import useUserRequest from '../../../../hooks/auth/useUserRequest'
+import { useQuery } from 'react-query'
 
-const Friend = ({addUser}) => {
+const Friend = ({ addUser }) => {
 
-    const { _id: currentUser } = useSelector(
-		(state) => state.user.currentUser.user,
-	);
-    const {mutate, isLoading, isError,status, error} = useMutationRequest(addFriend, {name:'friends'})
+    const { userId } = AuthProvider()
+    const privateRequest = useUserRequest()
+    const { mutate, isLoading, isError, status, error } = useMutationRequest(addFriend, { name: 'friends' })
 
-    const { data: userData } = useCallbackRequest({request:GetUser, id:currentUser, name:'user'})
+    const { data: userData } = useQuery(["user", userId], () => userService.getUser({ privateRequest, userId }));
 
-	const user = userData?.data?.data ?? {};
+    const user = userData?.data ?? {};
 
-    const handleMutate = useCallback(()=>{
-            mutate({
-                userId:currentUser, 
-                addUser: addUser?._id?.$oid
-            })
+    const handleMutate = useCallback(() => {
+        mutate({
+            userId,
+            addUser: addUser?._id?.$oid
+        })
     }, [])
-    
+
     const isFriend = isYourFriend(user?.friends, addUser?._id?.$oid)
 
-console.log({error})
-  return (
-    <div>
 
-        {/* if isLoading is true only */}
-        {isLoading && !isError && <SpinnerLoader/>}
-        
-        {!isLoading && !isError && (
-            <>
-            {isFriend && <p>friend</p>}
-            {!isFriend && (
-            <>
-            <p className='search-add-friend' onClick={( )=> handleMutate()}>añadir amigo</p>
-            </>
-             )}
-            </>
-        )}
+    return (
+        <div>
 
-        {!isLoading && isError && <p>error</p>}
-    </div>
-  )
+            {/* if isLoading is true only */}
+            {isLoading && !isError && <SpinnerLoader />}
+
+            {!isLoading && !isError && (
+                <>
+                    {isFriend && <p>friend</p>}
+                    {!isFriend && (
+                        <>
+                            <p className='search-add-friend' onClick={() => handleMutate()}>añadir amigo</p>
+                        </>
+                    )}
+                </>
+            )}
+
+            {!isLoading && isError && <p>error</p>}
+        </div>
+    )
 }
 
 export default Friend
