ca9383ad933f3f8e7a0563d4440c93a2659a017f
Jorge Ernesto <lower.joerge@gmail.com>
2023-10-13 14:57:14 -0700

    fixed with of comment loader

---
+++ client/src/components/MIDDLE/post/comments/ErrorMessage/ErrorMessageComment.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ErrorMessageComment.css'
+import ErrorButton from '../makeComment/styledComponentes/ErrorButton/ErrorButton'
+
+const ErrorMessageComment = ({ reset }) => {
+  return (
+    <div className='errorMessage-comment-container'>
+      <p>Something went wrong</p>
+      <span>try to research Comment, </span>
+      <ErrorButton reset={reset} />
+    </div>
+  )
+}
+
+export default ErrorMessageComment
