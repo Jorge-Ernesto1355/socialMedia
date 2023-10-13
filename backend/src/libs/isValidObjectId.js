af15a568db2d0832c0fc85fa66f1aa55836d08fa
Jorge Ernesto <lower.joerge@gmail.com>
2023-10-13 14:45:23 -0700

    added new configuration function isValidObjectId

--- backend/src/libs/isValidObjectId.js
+++ backend/src/libs/isValidObjectId.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 
-module.exports = async function validateObjectId(options, modelName) {
+module.exports = async function validateObjectId(query, options) {
   try {
-   
 
     if(typeof options !== 'object') throw new Error('options is not an object')
    
-    if (!mongoose.models[modelName]) throw new Error("model not found");
+    if (!mongoose.models[options.model]) throw new Error("model not found");
 
-    const document = await mongoose.models[modelName].findOne(options);
+    const document = await mongoose.models[options.model].findOne(query).select(options.select)
 
     if(document) return document
     
