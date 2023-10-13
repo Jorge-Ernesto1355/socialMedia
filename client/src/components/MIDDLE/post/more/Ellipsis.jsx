b5c807f47c4646aba1911bb350d9554cf17cb7ea
Jorge Ernesto <lower.joerge@gmail.com>
2023-10-13 14:59:19 -0700

    fixed some implementation of ellipsi

--- client/src/components/MIDDLE/post/more/Ellipsis.jsx
+++ client/src/components/MIDDLE/post/more/Ellipsis.jsx
@@ -1,32 +1,30 @@
 
 import "./Ellipsis.css";
 
-// icons
-import { useSelector } from "react-redux";
 import { useQuery } from "react-query";
-import GetUser from "../../../../services/GetUser.service";
 import EllipsiOwner from "./ellipsiOwnUser/EllipsiOwner";
 import EllipsiNormalUser from "./EllipsiNormalUser";
+import AuthProvider from "../../../../zustand/AuthProvider";
+import userService from "../../../../services/UserService";
+import useUserRequest from "../../../../hooks/auth/useUserRequest";
+import { ToastContainer } from "react-toastify";
 const EllipsisPost = ({ isOpen, userId, postId, handleCloseEllipsi }) => {
 
   // userId is the  user that has uploaded the post that we trying to config
   // currentUser is our id, is our own user
 
-  const { _id: currentUser } = useSelector(
-    (state) => state.user.currentUser.user,
-  );
+  const { userId: currentUser } = AuthProvider()
+  const privateRequest = useUserRequest()
 
-  const { data: userData, isLoading } = useQuery(["user", userId], () => GetUser(userId));
+  const { data: userData, isLoading, } = useQuery(["user", userId], () => userService.getUser({ privateRequest, userId }));
 
-  const user = userData?.data?.data ?? {};
-
-
+  const user = userData?.data ?? {};
 
 
   return (
     <>
       {currentUser === userId ? <EllipsiOwner postId={postId} isOpen={isOpen} username={user.username ?? ''} handleCloseEllipsi={handleCloseEllipsi} isLoading={isLoading} /> : <EllipsiNormalUser postId={postId} isOpen={isOpen} isLoading={isLoading} username={user.username ?? ''} />}
-
+      <ToastContainer hideProgressBar={true} />
     </>
   );
 };
--- client/src/components/MIDDLE/post/more/ellipsiOwnUser/EllipsiOwner.jsx
+++ client/src/components/MIDDLE/post/more/ellipsiOwnUser/EllipsiOwner.jsx
@@ -1,30 +1,10 @@
 import { motion } from "framer-motion";
 import { variantsMotion } from "../../../../../utilities/variantsMotion";
-import trash from '../icons/basura.png'
-import { DeletePost, EditPostMutation } from "./RequestOptionsOwnUser";
-import { useCallback } from "react";
-import { useSelector } from "react-redux";
-import SpinnerLoader from "../../../../../stylesComponents/spinnerLoader/SpinnerLoader";
-import ErrorButton from "../../comments/makeComment/styledComponentes/ErrorButton/ErrorButton";
 import EditPostConfirmationModal from "./editPost/EditPostIconConfirmationModal";
+import DeletePost from "./deletePost/DeletePost";
+
+
 const EllipsiOwner = ({ isOpen, handleCloseEllipsi, username = '', isLoading, postId }) => {
-
-  const { _id: currentUser } = useSelector(
-    (state) => state.user.currentUser.user,
-  );
-
-  const { mutateDelete, isLoadingMutationDelete, isErrorDeletePost, resetDelete } = DeletePost()
-
-  const { mutateEdit, isErrorEditPost, isLoadingMutationEdit } = EditPostMutation()
-
-  const handleDelete = useCallback(() => {
-
-    mutateDelete({
-      postId,
-      userId: currentUser
-    })
-
-  }, [])
 
 
   return (
@@ -34,18 +14,9 @@ const EllipsiOwner = ({ isOpen, handleCloseEllipsi, username = '', isLoading, po
       animate={`${isOpen ? "show" : "hide"}`}
       className="ellipsiPost-container"
     >
-      {isLoadingMutationDelete && <SpinnerLoader />}
-      {!isLoadingMutationDelete && (
-        <>
-          <li className="ellipsiPost-item" onClick={() => handleDelete()}>
+      <DeletePost postId={postId} />
+      <EditPostConfirmationModal postId={postId} handleCloseEllipsi={handleCloseEllipsi} />
 
-            <img src={trash} alt="icon delete post" />
-            {isErrorDeletePost && <ErrorButton reset={resetDelete} />}
-            <h4 className="ellipsiPost-text">Delete post</h4>
-          </li>
-          <EditPostConfirmationModal postId={postId} handleCloseEllipsi={handleCloseEllipsi} />
-        </>
-      )}
     </motion.ul>
   )
 }
