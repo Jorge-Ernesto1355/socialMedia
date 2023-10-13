92ba2d411ec803d1288c4ce636f4a1c6b6625e48
Jorge Ernesto <lower.joerge@gmail.com>
2023-10-13 15:10:56 -0700

    added componentStateHandler

--- client/src/hooks/stateManagmentComponent/ComponentStateHandler.jsx
+++ client/src/hooks/stateManagmentComponent/ComponentStateHandler.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import './ComponentStateHandler.css'
 
 const ComponentStateHandler = (props) => {
-    console.log(props)
+
     return (
         <div className='Component-state-handler-container'>
 
             {/* if IsLoading and isError is false it will display the Loader */}
             <div className='loading-stateManagment'>
-            {props.isLoading && !props.isError && (
-                props.LoaderComponent
-            )}
+                {props.isLoading && !props.isError && (
+                    props.Loader
+                )}
             </div>
 
             {/* if isLoading is false and isError es false too it will display the children */}
@@ -20,15 +20,15 @@ const ComponentStateHandler = (props) => {
 
             {/* if there's an error it will display an error message */}
             <div className='error-stateManagment'>
-            {!props.isLoading && props.isError && (
-                props.ErrorMessageComponent
-            )}
+                {!props.isLoading && props.isError && (
+                    props.ErrorMessageComponent
+                )}
             </div>
 
             {
                 !props.isLoading && !props.isError && (
                     <>
-                        {props.items <= 0 && <p>be the best</p> }
+                        {props.items <= 0 && <p>be the best</p>}
                     </>
                 )
             }
