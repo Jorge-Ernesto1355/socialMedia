ab96ced36925a20c5f55cb4ba9c98b6f48370669
Jorge Ernesto <lower.joerge@gmail.com>
2023-10-13 15:05:41 -0700

    deleted some functions

--- client/src/components/Navbar/Search/user/User.jsx
+++ client/src/components/Navbar/Search/user/User.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import './User.css'
 import rem from '../../../../assets/rem.jpg'
-import rightArrow from '../icons/rightArrow.png'
-import useMutationRequest from '../../../../hooks/useMutationRequest'
-import Friend from './Friend'
-const User = ({hit, itemsProps}) => {
 
-  
+import Friend from './Friend'
+const User = ({ hit, itemsProps }) => {
+
+
   return (
     <li className='search-user-container' {...itemsProps}>
-       
-        <img className='search-user-img' src={hit?.imageProfile?.url ?? rem} alt="" />
-       <div className='user-information-container'>
-       <h5 className='user-username'>{hit?.username}</h5>
-       <Friend addUser={hit}/>
-       </div>
+      <img className='search-user-img' src={hit?.imageProfile?.url ?? rem} alt="" />
+      <div className='user-information-container'>
+        <h5 className='user-username'>{hit?.username}</h5>
+        <Friend addUser={hit} />
+      </div>
     </li>
   )
 }
