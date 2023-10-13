4b3a637f6680267431b0b9f005ca4497e9bb9d15
Jorge Ernesto <lower.joerge@gmail.com>
2023-10-13 14:47:41 -0700

    deleted some ineccesary keys

--- backend/src/Post/application/validations/PostSchema.js
+++ backend/src/Post/application/validations/PostSchema.js
@@ -15,27 +15,8 @@ const PostSchema = z.object({
       required_error: "description is required",
     })
     .max(200),
-  image: z
-    .object({
-      url: z.string().optional(),
-      public_id: z.string().optional(),
-    })
-    .optional(),
-  comments: z.array(CommentSchema).optional(),
-  shares: z.array(z.string()).optional(),
-  votes: z.array(VoteSchema).optional(),
-  postShared: z.string().optional(),
-  reactions: z
-    .object({
-      gusta: z.array(ReactionSchema),
-      encanta: z.array(ReactionSchema),
-      asombra: z.array(ReactionSchema),
-      divierte: z.array(ReactionSchema),
-      entristece: z.array(ReactionSchema),
-    })
-    .optional(),
-  group: z.string().optional(),
-  edit: z.boolean().optional(),
+
+ 
 });
 
 function validatePost(object) {
