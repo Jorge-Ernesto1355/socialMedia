cf128b2ec4c52aa19163e1716b750aed1a979825
Jorge Ernesto <lower.joerge@gmail.com>
2023-10-13 15:08:31 -0700

    added loader friend request

---
+++ client/src/components/RIGHT/FriendsRequest/Loader/LoaderFriendRequest.jsx
@@ -0,0 +1,17 @@
+
+
+
+import React from 'react'
+import FriendsRequestLoader from '../FriendsRequestLoader'
+
+const LoaderFriendRequest = () => {
+    return (
+        <>
+            <FriendsRequestLoader />
+            <FriendsRequestLoader />
+
+        </>
+    )
+}
+
+export default LoaderFriendRequest
