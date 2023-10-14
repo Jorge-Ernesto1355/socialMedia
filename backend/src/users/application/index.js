35592216a2030b77b84fee5df52083427f791bf2
JorgeLap <jorgais.ernesto@gmail.com>
2023-10-13 17:37:11 -0700

    fixed some bugs

--- backend/src/users/application/index.js
+++ backend/src/users/application/index.js
@@ -5,36 +5,34 @@ const getPosts = require("./find/FindUserPosts");
 const AcceptFriends = require("./makeFriends/friends/AcceptFriends");
 const addFriend = require("./makeFriends/friends/AddFriends");
 const deleteFriend = require("../application/makeFriends/friends/deleteFriends");
-const UploadProfilePicture = require("../application/uploadImages/uploadProfilePicture");
-const UploadCover = require("../application/uploadImages/uploadCover");
+const UploadImageProfile = require("../application/uploadImages/UploadImageProfile");
 const addRelationShip = require("../application/makeFriends/relationship/addRelationShip");
 const AcceptRelationShip = require("../application/makeFriends/relationship/AcceptRelationShip");
 const deleteRelationShip = require("../application/makeFriends/relationship/deleteRelationShip");
 const giveRoles = require("../../roles/application/giveRoles");
 const deleteRoles = require("../../roles/application/removeRoles");
 const getRequestFriends = require("../application/makeFriends/friends/getRequestFriend");
-const deleteUser = require('./Delete/DeleteUserById')
-
+const deleteUser = require("./Delete/DeleteUserById");
+const getUsersTagged = require("./find/getUsersTagged");
 module.exports = {
-  query:{
-   
+  query: {
     getUsers,
     FindUserById,
     getFriends,
     getPosts,
     getRequestFriends,
+    getUsersTagged,
   },
-  mutation:{
+  mutation: {
     AcceptFriends,
-    addFriend, 
+    addFriend,
     deleteFriend,
-    UploadCover,
-    UploadProfilePicture,
+    UploadImageProfile,
     addRelationShip,
     deleteRelationShip,
     AcceptRelationShip,
-    giveRoles, 
-    deleteRoles, 
-    deleteUser
-  }
+    giveRoles,
+    deleteRoles,
+    deleteUser,
+  },
 };
