ff3be3a53f4e23ce82e8752a8d1a213854222c62
Jorge Ernesto <lower.joerge@gmail.com>
2023-10-15 21:22:26 -0700

    added component actions to message

--- client/src/components/RIGHT/Messages/MessageBox/actions/MessageBoxActions.jsx
+++ client/src/components/RIGHT/Messages/MessageBox/actions/MessageBoxActions.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './MessageBoxActions.css'
+import ImgInputFile from '../../../../../stylesComponents/ImgInputFile/ImgInputFile'
+import sendButton from '../../icons/paperPlaneBlue.png'
 
 const MessageBoxActions = () => {
     return (
-        <div>MessageBoxActions</div>
+        <div className='MessageBox-actions-container'>
+            <form className='MessageBox-actions-form'>
+                <input type="text" placeholder='Aa' className='MessageBox-actions-input' />
+            </form>
+            <div className='MessageBox-actions-InputFile'>
+                <ImgInputFile />
+            </div>
+            <button className='MessageBox-sendButton'>
+                <img src={sendButton} alt="" />
+            </button>
+
+
+        </div>
     )
 }
 
