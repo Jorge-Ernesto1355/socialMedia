0563e32364ddc76058f1999fe7950371278df7da
Jorge Ernesto <lower.joerge@gmail.com>
2023-10-15 21:22:47 -0700

    added component header to boxMessage

--- client/src/components/RIGHT/Messages/MessageBox/header/MessageBoxHeader.jsx
+++ client/src/components/RIGHT/Messages/MessageBox/header/MessageBoxHeader.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import './MessageBoxHeader.css'
+import rem from '../../../../../assets/rem.jpg'
+import cross from '../../../../../assets/cross.png'
+import minimize from '../../icons/minimize.png'
 const MessageBoxHeader = () => {
     return (
-        <div>header</div>
+        <div className='MessageBox-header-container'>
+            <div className='MessageBox-header-info'>
+                <div>
+                    <img className='profile-photo' src={rem} alt="profile info " />
+                </div>
+                <div className='header-username-container'>
+                    <h5 className=''>Jorge Ernesto</h5>
+                    <p className='online-status'>Activo(a) ahora</p>
+                </div>
+            </div>
+            <div className='MessageBox-header-actions'>
+                <img className='minimize-icon' src={minimize} alt="" />
+                <img className='cross-icon' src={cross} alt="" />
+            </div>
+
+
+        </div>
     )
 }
 
