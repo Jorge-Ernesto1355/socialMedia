2ef026cde2f0b1ae2139bc3af0852ff7ac932b51
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-07 07:41:47 -0700

    added new options

--- backend/src/libs/isValidObjectId.js
+++ backend/src/libs/isValidObjectId.js
@@ -1,22 +1,24 @@
 const mongoose = require("mongoose");
+const { DocumentNotFound } = require("../handleErros/errors");
 
 module.exports = async function validateObjectId(query, options) {
   try {
-   
+    if (typeof options !== "object")
+      throw new Error("options is not an object");
 
-    if(typeof options !== 'object') throw new Error('options is not an object')
-   
     if (!mongoose.models[options.model]) throw new Error("model not found");
 
-    const document = await mongoose.models[options.model].findOne(query).select(options.select)
+    const document = await mongoose.models[options.model]
+      .findOne(query)
+      .select(options.select);
 
     if (document) return document;
 
-    if (!document) throw new Error("document not found");
+    if (!document) throw new DocumentNotFound("document not found");
   } catch (error) {
     return {
       error,
-      message: error.message, 
+      message: error.message,
     };
   }
-}
+};
