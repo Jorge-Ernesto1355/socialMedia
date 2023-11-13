ef9362d088cfa991a11b67ea6c830b36af50ed02
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-13 08:10:30 -0700

    build: new model conversation
    conversation model that will help us to handle the send of messages

--- backend/src/messages/domain/Conversation.js
+++ backend/src/messages/domain/Conversation.js
@@ -1,5 +1,38 @@
+const { default: mongoose } = require("mongoose");
 const { Schema, model } = require("mongoose");
-
-const Conversation = new Schema({});
+const Conversation = new Schema({
+  participants: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+  ],
+  messages: [
+    {
+      to: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+      },
+      from: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+      },
+      type: {
+        type: String,
+        enum: ["Text", "Media", "Document", "Link"],
+      },
+      created_at: {
+        type: Date,
+        default: Date.now(),
+      },
+      text: {
+        type: String,
+      },
+      file: {
+        type: String,
+      },
+    },
+  ],
+});
 
 module.exports = model("Conversation", Conversation);
