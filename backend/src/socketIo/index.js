a56d7b6ec82774a84ee0c8f24808853bc12fce21
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-12 18:14:46 -0700

    refactor(security): :lock: middleware socket io
    implemented middleware to handle the sessions with socketIO through jwt to validate if is valid token

--- backend/src/socketIo/index.js
+++ backend/src/socketIo/index.js
@@ -1,9 +1,38 @@
-const { Server } = require("socket.io")
+const { Server: SocketServer } = require("socket.io");
+const jwt = require("jsonwebtoken");
+const { ACCESS_TOKEN_SECRET } = require("../dotenv");
 
-module.exports =  function socketIo(server) {
-  const io = new Server(server);
-  io.on("connection", function (socket) {
-    console.log(socket.id);
-    console.log("Made socket connection");
+module.exports = function socketIo(server) {
+  const io = new SocketServer(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
   });
-}
+
+  io.use((socket, next) => {
+    const token = socket.handshake.token;
+    // Implementa la validación del token u otros datos
+    try {
+      const isValid = jwt.verify(token, ACCESS_TOKEN_SECRET);
+      if (isValid) {
+        return next();
+      }
+      return next(new Error("Autenticación fallida"));
+    } catch (error) {
+      return next(new Error("Something went wrong socket"));
+    }
+  });
+
+  io.on("connection", function (socket) {
+    console.log("socket");
+    socket.on("disconnect", () => {
+      console.log("Usuario desconectado");
+    });
+
+    socket.on("error", (error) => {
+      console.error("Error en la conexión del usuario:", error);
+      // Implementa lógica de manejo de errores en el servidor
+    });
+  });
+};
