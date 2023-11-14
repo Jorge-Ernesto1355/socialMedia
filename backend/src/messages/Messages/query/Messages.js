61c2bd83c67df7d8aa4b9457226769e82546f6c8
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-13 19:12:03 -0700

    chore(infrastructure): :sparkles: infrastructure get messages
    pagination messages with paginate mongoose through infrastructure message controller

--- backend/src/messages/Messages/query/Messages.js
+++ backend/src/messages/Messages/query/Messages.js
@@ -1,11 +1,21 @@
+const MessageService = require("../../MessageService");
+
 const Messages = async (req, res) => {
-  const messages = await MessageService.messages({});
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const page = parseInt(req.query.page, 10) || 1;
+  const { conversationId } = req.params;
+
+  const messages = await MessageService.messages({
+    conversationId,
+    limit,
+    page,
+  });
 
   if (messages?.error) {
     return res.status(500).json({ error: messages?.message });
   }
 
-  return messages;
+  return res.status(200).json(messages);
 };
 
 module.exports = Messages;
