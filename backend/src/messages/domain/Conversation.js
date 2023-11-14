d15bc417f0185ef5c1967b705bce6f90fc14b589
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-13 19:13:01 -0700

    chore: deleted message to make it's own model

--- backend/src/messages/domain/Conversation.js
+++ backend/src/messages/domain/Conversation.js
@@ -7,32 +7,6 @@ const Conversation = new Schema({
       ref: "User",
     },
   ],
-  messages: [
-    {
-      to: {
-        type: mongoose.Schema.ObjectId,
-        ref: "User",
-      },
-      from: {
-        type: mongoose.Schema.ObjectId,
-        ref: "User",
-      },
-      type: {
-        type: String,
-        enum: ["Text", "Media", "Document", "Link"],
-      },
-      created_at: {
-        type: Date,
-        default: Date.now(),
-      },
-      text: {
-        type: String,
-      },
-      file: {
-        type: String,
-      },
-    },
-  ],
 });
 
 module.exports = model("Conversation", Conversation);
