7c7ec2f40022cd0ddd46835755f192a0d28505b1
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-13 19:08:46 -0700

    chore(infrastructure): :sparkles: message action make message
    provides autoComplete input and images to make a new message with socket

--- client/src/components/RIGHT/Messages/MessageBox/actions/MessageBoxActions.jsx
+++ client/src/components/RIGHT/Messages/MessageBox/actions/MessageBoxActions.jsx
@@ -1,25 +1,45 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import './MessageBoxActions.css'
 import ImgInputFile from '../../../../../stylesComponents/ImgInputFile/ImgInputFile'
 import sendButton from '../../icons/paperPlaneBlue.png'
-import io from 'socket.io-client'
-const socket = io('http://localhost:3002')
+import { useStore } from '../../../../../hooks/useStore/useStore'
+import AutoComplete from '../../../../Autocomplete/AutoComplete'
+import AuthProvider from '../../../../../zustand/AuthProvider'
+import { useSocket } from '../../../../../hooks/useSocket'
 
 
-const MessageBoxActions = () => {
-    const onClick = () => {
-        socket.emit('message', { message: 'jorge gupao' })
-    }
+
+const MessageBoxActions = ({friendId, conversation}) => {
+    const {userId} = AuthProvider()
+    const { store, set, get } = useStore();
+    const socket = useSocket()
+
+
+    const onClick = useCallback(() => {
+
+        if(get().length <= 0) return 
+        
+        socket?.emit('new-message', {to:friendId, from:userId, conversationId:conversation?._id, message:get()})
+    }, [get()])
 
 
     return (
         <div className='MessageBox-actions-container'>
             <form className='MessageBox-actions-form'>
-                <input type="text" placeholder='Aa' className='MessageBox-actions-input' />
+            {store && (
+            <AutoComplete
+              placeholder={"Escribe un comentario..."}
+              rows={1}
+              cols={28}
+              ref={store}
+              set={set}
+              stateValue={get}
+            />
+          )} 
             </form>
             <div className='MessageBox-actions-InputFile'>
                 <ImgInputFile />
-            </div>
+            </div>  
             <button className='MessageBox-sendButton' onClick={() => onClick()}>
                 <img src={sendButton} alt="" />
             </button>
