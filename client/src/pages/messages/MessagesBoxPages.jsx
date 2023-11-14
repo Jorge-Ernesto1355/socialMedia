2cdedb2ddc3dae3a988dbe9fa57b51ede283d842
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-13 18:17:57 -0700

    feat: :sparkles: listeners socket to open conversation
    listener socket to handle the differents conversation

--- client/src/pages/messages/MessagesBoxPages.jsx
+++ client/src/pages/messages/MessagesBoxPages.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './MessageBoxPage.css'
 import ReactPortal from '../../components/modal/ReactPortal'
 import MessageBox from '../../components/RIGHT/messages/MessageBox/BoxMessage'
+import { useSocket } from '../../hooks/useSocket'
+import BoxMessagesStore from '../../zustand/BoxMessagesStore'
 
 
 
 const MessageBoxPage = () => {
 
+  const socket = useSocket()
+  const {setBoxMessages, boxMessages} = BoxMessagesStore()
+
+
+  useEffect(()=>{
+
+    socket?.on('open-conversation', (conversation)=>{
+      setBoxMessages(conversation)
+    })
+
+    return ()=>{
+       socket?.off('open-conversation')
+    }
+    
+  }, [socket])
+
   return (
     <ReactPortal wrapperId={'Message-box-popup'}>
       <div className='boxMessages-container'>
         <ul className='boxMessages-row'>
-          <MessageBox />
-
+          {boxMessages?.map((conversation)=> (
+            <MessageBox key={`messagebox-key=${conversation?._id}`} conversation={conversation}/>
+          ))}
         </ul>
       </div>
     </ReactPortal>
