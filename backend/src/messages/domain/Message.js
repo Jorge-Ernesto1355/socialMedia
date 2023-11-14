cc547423aef5eae00f7fbdb4b695a661a98f026c
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-13 19:13:36 -0700

    feat(infrastructure): new model Message

--- backend/src/messages/domain/Message.js
+++ backend/src/messages/domain/Message.js
@@ -1,5 +1,36 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, default: mongoose } = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
+const Message = new Schema(
+  {
+    conversationId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Conversation",
+    },
+    to: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+    from: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+    type: {
+      type: String,
+      enum: ["Text", "Media", "Document", "Link"],
+    },
+    text: {
+      type: String,
+    },
+    file: {
+      url: String,
+      public_id: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Message = new Schema({});
+Message.plugin(mongoosePaginate);
 
 module.exports = model("Message", Message);
