543e0c6f3691f47cb2873a9ae52b4806fadcb0ee
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-16 22:53:01 -0700

    changed names of controllers

--- backend/src/messages/Messages/index.js
+++ backend/src/messages/Messages/index.js
@@ -1,16 +1,18 @@
 //mutation
-const message = require("./mutations/Message");
+const createMessage = require("./mutations/Message");
 const deleteMessage = require("./mutations/DeleteMessage");
 const updateMessage = require("./mutations/updateMessage");
 //query
 const messages = require("./query/Messages");
+const message = require("./query/message");
 
 module.exports = {
   query: {
     messages,
+    message,
   },
   mutation: {
-    message,
+    createMessage,
     deleteMessage,
     updateMessage,
   },
