e948fb42460b7a158cc7a34ec4687df7990bfab2
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-16 22:48:39 -0700

    feat: implementation of useHover hook

--- client/src/components/Reaction/LikeMessage.jsx
+++ client/src/components/Reaction/LikeMessage.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./likePost.css";
 
 import emoji from '../RIGHT/icons/sonreir.png'
+import useHover from "../../hooks/useHover";
+
+const LikeMessage = ({ reactionType, setShowReactions, isVisible}) => {
+
+   const {hovered, show } = useHover()
+
+   console.log(isVisible)
 
 
-const LikeMessage = ({ reactionType, setShowReactions }) => {
+   useEffect(()=>{
+    setShowReactions(show)
+    isVisible()
+   }, [show])
+
+   
+   
   return (
-    <div className="ratingIcon">
+    <div className="ratingIcon" >
       
         <div
           className="reaction-div"
-          onClick={() => setShowReactions((prev) => !prev)}
-          onMouseEnter={() => setShowReactions(true)}
+          onClick={() => {
+            setShowReactions((prev) => !prev)
+            isVisible()
+          }}
+          onMouseEnter={() => hovered(true)}
         >
           <p className='popover-text-message'>React</p>
                     <span><img src={emoji} alt="react" className='popover-img-message' /></span>
