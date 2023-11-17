cc9ca7d809c1e9be574ddca98fa071363cf2c912
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-16 22:48:18 -0700

    feat: implementation of useHover Hook with the options reactions

--- client/src/components/Reaction/LIkePost.jsx
+++ client/src/components/Reaction/LIkePost.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./likePost.css";
 import { motion } from "framer-motion";
 import like from "../../assets/likePost.png";
 import { objetsImgs } from "../MIDDLE/post/post/objectImg";
+import useHover from "../../hooks/useHover";
 
 
-const LikePost = ({ reactionType, setShowReactions }) => {
+const LikePost = ({ reactionType, setShowReactions, isVisible }) => {
+
+  
+  const {hovered, show} = useHover()
+ 
+  useEffect(()=>{
+   setShowReactions(show)
+   isVisible()
+  }, [show])
+
   return (
-    <div className="ratingIcon">
+    <div className="ratingIcon" onMouseEnter={() => hovered(true)}>
       {reactionType === null ? (
         <div
           className="reaction-div"
-          onClick={() => setShowReactions((prev) => !prev)}
-          onMouseEnter={() => setShowReactions(true)}
+          onClick={() => {
+            setShowReactions((prev) => !prev)
+            isVisible()
+          }}
+          onMouseEnter={() => hovered(true)}
         >
           <img className="icon" src={like} alt="like-post" />
           <span className="reaction-text">me gusta</span>
         </div>
       ) : (
-        <div className="actionsActivated">
+        <div className="actionsActivated" onMouseEnter={() => hovered(true)}>
           <motion.img
             initial={{ scale: 0 }}
             animate={{ scale: 1.1 }}
@@ -27,7 +40,7 @@ const LikePost = ({ reactionType, setShowReactions }) => {
           />
           <span
             className={reactionType}
-            onClick={() => setShowReactions((prev) => !prev)}
+            onClick={() => hovered(true)}
           >
             me {reactionType}
           </span>
