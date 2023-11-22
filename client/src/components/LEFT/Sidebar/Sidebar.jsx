90369aae7280a5ed9e62d701d1f872b864e30cdf
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-22 07:41:19 -0700

    chore(fire): delete icons from react-icons depende

--- client/src/components/LEFT/Sidebar/Sidebar.jsx
+++ client/src/components/LEFT/Sidebar/Sidebar.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import "./Sidebar.css";
 
-import { AiOutlineHome, AiOutlineMessage } from "react-icons/ai";
-import { MdOutlineExplore } from "react-icons/md";
-import { IoIosNotificationsOutline } from "react-icons/io";
-import { BsBookmark } from "react-icons/bs";
-import { HiOutlineColorSwatch } from "react-icons/hi";
-import { FiSettings } from "react-icons/fi";
+
 import { Link } from "react-router-dom";
 import Theme from "../../Theme/Theme";
 
@@ -35,14 +30,13 @@ const Sidebar = () => {
     <div className="sidebar">
       <a className="menu-item">
         <span>
-          <AiOutlineHome />
+        
         </span>
         <h3>Home</h3>
       </a>
 
       <a className="menu-item">
         <span>
-          <MdOutlineExplore />
         </span>
         <h3>Explore</h3>
       </a>
@@ -50,21 +44,21 @@ const Sidebar = () => {
 
       <a className="menu-item" >
         <span>
-          <AiOutlineMessage />
+        
         </span>
         <i>9</i>
         <h3>Mensajes</h3>
       </a>
       <Link className="menu-item">
         <span>
-          <BsBookmark />
+        
         </span>
         <h3>Favoritos</h3>
       </Link>
 
       <a className="menu-item" >
         <span>
-          <HiOutlineColorSwatch />
+         
         </span>
         <Theme />
       </a>
