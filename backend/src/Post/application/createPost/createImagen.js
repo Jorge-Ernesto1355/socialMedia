91a9663182223222f90986d0c6fd7aeca07d5146
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-22 07:46:35 -0700

    create imagen

--- backend/src/Post/application/createPost/createImagen.js
+++ backend/src/Post/application/createPost/createImagen.js
@@ -4,9 +4,9 @@ const fs = require("fs-extra");
 const createImagen = async (files) => {
   if (!files?.image) return null;
 
-  const result = await uploadImage(req.files.image.tempFilePath);
+  const result = await uploadImage(files?.image?.tempFilePath);
 
-  await fs.remove(req.files.image.tempFilePath);
+  await fs.remove(files?.image?.tempFilePath);
   return (image = {
     url: result.secure_url,
     public_id: result.public_id,
