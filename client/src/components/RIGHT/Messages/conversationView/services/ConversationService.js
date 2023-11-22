e8f63777916f55013f5fec3b56177a183d3079cc
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-22 07:14:09 -0700

    feat: add requests to Converation service client

--- client/src/components/RIGHT/Messages/conversationView/services/ConversationService.js
+++ client/src/components/RIGHT/Messages/conversationView/services/ConversationService.js
@@ -1,3 +1,4 @@
+/* eslint-disable lines-between-class-members */
 import { ObjectErrosName } from "../../../../../utilities/ObjectErrorsName";
 
 export default class ConversationService {
@@ -5,7 +6,7 @@ export default class ConversationService {
     try {
       if (!privateRequest)
         throw new Error(ObjectErrosName.PrivateRequestDoesNotExitst);
-      return await privateRequest.get(`/conversation/${userId}`);
+      return await privateRequest.get(`/conversation/all/${userId}`);
     } catch (error) {
       return {
         error,
@@ -19,6 +20,32 @@ export default class ConversationService {
       if (!privateRequest)
         throw new Error(ObjectErrosName.PrivateRequestDoesNotExitst);
       return await privateRequest.get(`/message/${conversationId}/lastMessage`);
+    } catch (error) {
+      return {
+        error,
+        message: error.message,
+      };
+    }
+  }
+
+  static async Conversation({ privateRequest, id: conversationId }) {
+    try {
+      if (!privateRequest)
+        throw new Error(ObjectErrosName.PrivateRequestDoesNotExitst);
+      return await privateRequest.get(`/conversation/one/${conversationId}`);
+    } catch (error) {
+      return {
+        error,
+        message: error.message,
+      };
+    }
+  }
+
+  static async blockContact({ privateRequest, id: conversationId }) {
+    try {
+      if (!privateRequest)
+        throw new Error(ObjectErrosName.PrivateRequestDoesNotExitst);
+      return await privateRequest.put(`/conversation/block/${conversationId}`);
     } catch (error) {
       return {
         error,
