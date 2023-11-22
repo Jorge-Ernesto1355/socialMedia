f7e3784f068cbfbc43e287c44de81dd41ad3ac4e
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-22 07:08:11 -0700

    feat: conversation route
    add controllers to get conversations and others

--- backend/src/messages/infrastructure/Conversation.routes.js
+++ backend/src/messages/infrastructure/Conversation.routes.js
@@ -2,8 +2,12 @@ const { Router } = require("express");
 const index = require("../Conversations/index");
 const router = Router();
 
-router.get("/", index.query.getConversations);
+router.get("/all/:userId", index.query.getConversations);
 
 router.post("/", index.mutation.Conversation);
+
+router.get("/one/:conversationId", index.query.getConversation);
+
+router.put("/block/:conversationId", index.mutation.blockContact);
 
 module.exports = router;
