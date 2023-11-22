52adb9554a4dfa6028016980c1f38d9b113f3426
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-22 07:15:53 -0700

    feat(infrastructure): add controller to index message

--- backend/src/messages/Messages/index.js
+++ backend/src/messages/Messages/index.js
@@ -2,14 +2,17 @@
 const createMessage = require("./mutations/Message");
 const deleteMessage = require("./mutations/DeleteMessage");
 const updateMessage = require("./mutations/updateMessage");
+
 //query
 const messages = require("./query/Messages");
 const message = require("./query/message");
+const lastMessage = require("./query/lastMessage");
 
 module.exports = {
   query: {
     messages,
     message,
+    lastMessage,
   },
   mutation: {
     createMessage,
