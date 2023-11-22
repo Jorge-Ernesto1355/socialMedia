a882f8da6cdb669bf66ef3e552ec184f6e1c578f
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-22 07:45:36 -0700

    getUserById controller

--- backend/src/users/application/find/FindUserById.js
+++ backend/src/users/application/find/FindUserById.js
@@ -1,21 +1,15 @@
-const User = require("../../domain/UserModel")
-const userService = require("../../userService")
+const userService = require("../../userService");
 
+const FindUserById = async (req, res) => {
+  const { userId } = req.params;
 
+  const user = await userService.get({ userId });
 
-
-const FindUserById = async (req, res)=>{
-
-  const {userId} = req.params
-
-  const user = await userService.get({userId})
-
-  if(user?.error){
-    return res.status(500).json({message: user.message})
+  if (user?.error) {
+    return res.status(500).json({ message: user.message });
   }
 
-  return res.status(200).json(user)
-}
+  return res.status(200).json(user);
+};
 
-
-module.exports = FindUserById
+module.exports = FindUserById;
