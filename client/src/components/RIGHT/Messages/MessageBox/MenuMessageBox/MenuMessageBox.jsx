a183251fd1844b99fb2807155117dee8789ad8a1
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-22 06:51:19 -0700

    feat: :sparkles: menu conversation
    provides some options to conversations as block conve or call

---
+++ client/src/components/RIGHT/Messages/MessageBox/MenuMessageBox/MenuMessageBox.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Popover from '../../../../../hooks/Popover/Popover'
+import menu from '../../icons/menu.png'
+import call from '../../icons/llamada-telefonica.png'
+import camara from '../../icons/camara.png'
+
+import messenger from '../../icons/mensajero.png'
+import user from '../../icons/usuario.png'
+import './menuMessageBox.css'
+import BlockContact from './BlockContact'
+
+
+const MenuMessageBox = ({conversation}) => {
+  return (
+  
+    
+        <Popover trigger={<img src={menu} className='menu-message' alt="more"/>}>
+          <ul className='menuMessageBox-container'>
+            <li className='menuMessageBox-item'>
+                <img  className='menuMessageBox-img' src={messenger} alt="" />
+                <p className='menuMessageBox-text'>Abrir en Messenger</p>
+            </li>
+            <li className='menuMessageBox-item'>
+                <img className='menuMessageBox-img' src={user} alt="" />
+                <p className='menuMessageBox-text'>View Profile</p>
+            </li>
+            <li className='menuMessageBox-item'>
+                <img className='menuMessageBox-img' src={call} alt="" />
+                <p className='menuMessageBox-text'>Start call</p>
+            </li>
+            <li className='menuMessageBox-item'>
+                <img className='menuMessageBox-img' src={camara} alt="" />
+                <p className='menuMessageBox-text'>Start video call</p>
+            </li>
+            <BlockContact conversation={conversation}/>
+          </ul>
+        </Popover>
+    
+   
+  )
+}
+
+export default MenuMessageBox
