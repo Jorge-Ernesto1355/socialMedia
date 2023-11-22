ad2276a00c092e66cdf36699e84ed37ad26f526d
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-22 06:53:05 -0700

    feat(infrastructure): :sparkles: implementation of messages with infiniteScroll

--- client/src/components/RIGHT/Messages/MessageBox/body/MessageBoxBody.jsx
+++ client/src/components/RIGHT/Messages/MessageBox/body/MessageBoxBody.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import './MessageBoxBody.css'
 import Message from '../../Message/Message'
 import useInfiniteScroll from '../../../../../hooks/useInfiniteScroll/useInfiniteScroll'
@@ -8,14 +8,19 @@ import messageService from '../service/MessageService'
 import { useSocket } from '../../../../../hooks/useSocket'
 import { useQueryClient } from 'react-query'
 
+import SpinnerLoader from '../../../../../stylesComponents/spinnerLoader/SpinnerLoader'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import Loader from '../../../../../utilities/Loader'
+
 const MessageBoxBody = ({conversation}) => {
 
     
+    const messagesContainerRef = useRef();
     const privateRequest = useUserRequest()
     const {userId} = AuthProvider()
     const queryClient = useQueryClient()
     const socket = useSocket()
-    const {results, isLoading, isError} = useInfiniteScroll({
+    const {results, isLoading, isError, hasNextPage, fetchNextPage} = useInfiniteScroll({
         name:"messages", 
         request:messageService.messages,
         privateRequest, 
@@ -23,47 +28,78 @@ const MessageBoxBody = ({conversation}) => {
 
     })
 
-     useEffect(()=>{
+    useEffect(() => {
+        const handleNewMessage = (message) => {
+         
+      
+          queryClient.setQueryData(['messages', conversation?._id], (prevData) => {
+            const dataDocs = prevData?.pages[0]?.data?.docs ?? [];
+            const newDocs = [...dataDocs, message];
+            const { docs, ...restData } = prevData.pages[0].data;
+      
+            return {
+              pages: [
+                {
+                  data: {
+                    docs: newDocs,
+                    restData,
+                  },
+                },
+              ],
+            };
+          });
+        };
+      
+        socket?.on('new-message', handleNewMessage);
+      
+        // Scroll automático cuando hay un nuevo mensaje
+        messagesContainerRef.current.scrollIntoView({ behavior: 'smooth' });
+      
+        return () => {
+          socket.off('new-message', handleNewMessage);
+        };
+      }, [socket, queryClient, conversation]);
+      
+      
+      
 
-        socket?.on('new-message', async (message)=>{
-          
-            queryClient.setQueryData(['messages', conversation?._id], (newMessage)=>{
-              
-                const messageToAdd = structuredClone(message)
-                messageToAdd.preview = true
-                const dataDocs = newMessage?.pages[0]?.data?.docs ?? []
-                const newDocs = [...dataDocs, messageToAdd]
-                const {docs, ...restData}  = newMessage.pages[0].data 
-                
-                
-                return {
-                    pages: [
-                       {
-                        data:{
-                            docs:newDocs, 
-                            restData
-                        }
-                       }
-                    ]
-                }
-            })
 
-        })
-
-        return ()=>{
-            socket.off('new-message')
-        }
-
-     }, [])
+    
     return (
-        <div className='messages-container'>
-        {
+        <div className='messages-container'  style={{
+            height: 300,
+            overflow: 'auto',
+            display: 'flex',
+            flexDirection: 'column-reverse',
+          }} id="scrollableDiv"
+        >
+         <InfiniteScroll
+        dataLength={results.length}
+        next={fetchNextPage}
+        style={{ display: 'flex', flexDirection: 'column-reverse' }} 
+        inverse={true} //
+       
+        hasMore={hasNextPage || isLoading}
+        loader={<Loader/>}
+        scrollableTarget="scrollableDiv"
+         >
+         <>
+         {isLoading && <SpinnerLoader/>}
+          {isError && <>Error Message</>}
+            {!isLoading && !isError && (<>
+                {
             !!results && results?.map((message)=>{
                     const isMyMessage = userId !== message.to
                     return <Message key={`message-key=${message?._id}`} isMyMessage={isMyMessage} message={message}/>
             })
         }
-          
+            </>)}
+            <div ref={messagesContainerRef}></div>
+
+         </>
+         </InfiniteScroll>
+         
+
         </div>
     )
 }
