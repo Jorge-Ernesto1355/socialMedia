80828789aab0ee750a0792a932a5c4795c17082d
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-22 06:54:43 -0700

    feat(infrastructure): :sparkles: cloudinary service
    provides a method to upload and delete image

--- backend/src/libs/cloudynary.js
+++ backend/src/libs/cloudynary.js
@@ -1,44 +1,49 @@
-const {v2: cloudinary} = require('cloudinary')
-const exits = require('./exits')
+const { v2: cloudinary } = require("cloudinary");
+const exits = require("./exits");
 
 cloudinary.config({
-  cloud_name:"cbta", 
-  api_key:"911678697972225", 
-  api_secret:"k5TzOoP0uMgprtv1sDAprFTwtHo"
-
-})
-
+  cloud_name: "cbta",
+  api_key: "911678697972225",
+  api_secret: "k5TzOoP0uMgprtv1sDAprFTwtHo",
+});
 
 class cloudinaryService {
-  static async upload(object){
+  static async upload(object) {
     try {
-      exits(object)
-      const {filePath} = object
-      return await cloudinary.uploader.upload(filePath, {
-        folder:"prueba"
-      })
+      exits(object);
+      const { filePath } = object;
+
+      if (!filePath) return;
+      const resultImage = await cloudinary.uploader.upload(filePath, {
+        folder: "prueba",
+      });
+
+      const image = {
+        url: resultImage.secure_url,
+        public_id: resultImage.public_id,
+      };
+
+      return image;
     } catch (error) {
       return {
-        error, 
-        message: error.message
-      }
+        error,
+        message: error.message,
+      };
     }
   }
 
-  static async deleteImage(object){
+  static async deleteImage(object) {
     try {
-      exits(object)
-      const {imageId} = object 
-      await v2.uploader.destroy(imageId)
+      exits(object);
+      const { imageId } = object;
+      await v2.uploader.destroy(imageId);
     } catch (error) {
       return {
-        error, 
-        message: error.message
-      }
+        error,
+        message: error.message,
+      };
     }
   }
 }
 
-module.exports = cloudinaryService
-
-
+module.exports = cloudinaryService;
