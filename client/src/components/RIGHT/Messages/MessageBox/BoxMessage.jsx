8799886258dc0afb62727b5edc6ddc4bdbb165c1
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-22 07:04:07 -0700

    feat: add ComponentStateHandler to handle the loading and the error

--- client/src/components/RIGHT/Messages/MessageBox/BoxMessage.jsx
+++ client/src/components/RIGHT/Messages/MessageBox/BoxMessage.jsx
@@ -11,21 +11,28 @@ import BoxMessagesStore from '../../../../zustand/BoxMessagesStore'
 import {toast} from 'react-toastify'
 import { variantsMessageBox } from './variants'
 import AuthProvider from '../../../../zustand/AuthProvider'
+import { useCallbackRequest } from '../../../../hooks/useCallbackRequest/useCallbackRequest'
+import ConversationService from '../conversationView/services/ConversationService'
+import ComponentStateHandler from '../../../../hooks/stateManagmentComponent/ComponentStateHandler'
+import SpinnerLoader from '../../../../stylesComponents/spinnerLoader/SpinnerLoader'
 
 
-const BoxMessage = ({conversation}) => {
+const BoxMessage = ({conversationId}) => {
   
 
     const {userId} = AuthProvider()
     const [minimize, setMinimize] = useState(true)
     const privateRequest = useUserRequest()
     const {deleteConversation} = BoxMessagesStore() 
+    const {data:conversationData, isLoading:isLoadingConversation, isError:isErrorConversation} = useCallbackRequest({request:ConversationService.Conversation, name:"conversation", id:conversationId, privateRequest})
+    
+
+    const conversation = conversationData?.data ?? {}
    
     const friendId = conversation?.participants?.filter((participant)=> participant !==  userId)[0] ?? null
 
 
-    
-
+ 
     const { data: userData } = useQuery(["user", friendId], () => userService.getUser({ privateRequest, userId:friendId }), {
         onError:()=>{
             toast.error('something went wrong with your friend')
@@ -33,15 +40,15 @@ const BoxMessage = ({conversation}) => {
         }
     });
 
-   
-
-	const user = userData?.data ?? {};
+    	const user = userData?.data ?? {};
 
     return (
         <motion.div animate={`${minimize ? 'show' : 'hidden'}`} variants={variantsMessageBox} className='MessageBox-container'>
-            <MessageBoxHeader minimize={setMinimize} conversation={conversation} friend={user} />
+            <ComponentStateHandler isLoading={isLoadingConversation} isError={isErrorConversation} Loader={<SpinnerLoader/>} ErrorMessageComponent={<>errro Conversation</>} >
+            <MessageBoxHeader minimize={setMinimize} deleteConversation={deleteConversation} conversation={conversation} friend={user} />
             <MessageBoxBody conversation={conversation} />
             <MessageBoxActions conversation={conversation} friendId={friendId} userId={userId} />
+            </ComponentStateHandler>
         </motion.div>
     )
 }
