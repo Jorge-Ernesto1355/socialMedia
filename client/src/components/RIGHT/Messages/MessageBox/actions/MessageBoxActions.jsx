d764d7a79236f948d44d0ba9ae0bcc560560f39d
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-22 07:02:30 -0700

    feat: :sparkles: input and options to send a message with reply or image

--- client/src/components/RIGHT/Messages/MessageBox/actions/MessageBoxActions.jsx
+++ client/src/components/RIGHT/Messages/MessageBox/actions/MessageBoxActions.jsx
@@ -1,7 +1,7 @@
-import React, { useCallback } from 'react'
+/* eslint-disable no-constant-condition */
+import React, { useCallback, useEffect, useState } from 'react'
 import './MessageBoxActions.css'
 import ImgInputFile from '../../../../../stylesComponents/ImgInputFile/ImgInputFile'
-import sendButton from '../../icons/paperPlaneBlue.png'
 import { useStore } from '../../../../../hooks/useStore/useStore'
 import AutoComplete from '../../../../Autocomplete/AutoComplete'
 import AuthProvider from '../../../../../zustand/AuthProvider'
@@ -9,59 +9,178 @@ import { useSocket } from '../../../../../hooks/useSocket'
 import Reply from '../../Message/Reply/Reply'
 import BoxMessagesStore from '../../../../../zustand/BoxMessagesStore'
 import cross from '../../../../../assets/cross.png'
+import { useMutation,  useQueryClient } from 'react-query'
+import messageService from '../service/MessageService'
+import useUserRequest from '../../../../../hooks/auth/useUserRequest'
+import UseImagePreview from '../../../../../hooks/useImagePreview/useImagePreview'
+import LoaderVote from '../../../../MIDDLE/post/Votes/LoaderVote'
+import sendBlue from '../../icons/paperPlaneBlue.png'
+import sendGray from '../../icons/paperPlaneGray.png'
+import agregarPicture from '../../icons/agregar.png'
+import EmojiPickerComponent from '../../../../EmojiPicker/EmojiPicker'
+import emoji from '../../icons/feliz.png'
 
 
 const MessageBoxActions = ({friendId, conversation}) => {
     const {userId} = AuthProvider()
+    const privateRequest = useUserRequest()
+    
+    const [isOpen, setIsOpen] = useState(false)
     const { store, set, get } = useStore();
+    const { element, input: inputFile, clearImagePreview } = UseImagePreview()
     const socket = useSocket()
+    const queryClient = useQueryClient()
+    const messageKey = ['messages', conversation?._id]
     const {messageReply, deleteMessageReply} = BoxMessagesStore() 
+    const {mutateAsync, isLoading, isError} = useMutation({
+        mutationFn:messageService.createMessage, 
+        onMutate: async (message)=>{
 
+                await queryClient.cancelQueries(messageKey)
+
+                const previewsMessages = queryClient.getQueryData(messageKey,)
+
+                queryClient.setQueryData(messageKey, (newMessage)=>{
+              
+                    const {privateRequest, ...restMessage} = message
+                    const messageToAdd = structuredClone(restMessage)
+                    messageToAdd.preview = true
+                    const dataDocs = newMessage?.pages[0]?.data?.docs ?? []
+                    const newDocs = [...dataDocs, messageToAdd]
+                    const {docs, ...restData}  = newMessage.pages[0].data 
+                    
+                    return {
+                        pages: [
+                           {
+                            data:{
+                                docs:newDocs, 
+                                ...restData
+                            }
+                           }
+                        ]
+                    }
+                })
+
+                return {previewsMessages}
+        }, 
+        onError: (_error, _variables, context)=>{
+            if(context?.previewsMessages !== null)
+             queryClient.setQueryData(messageKey, context?.previewsMessages)
+                
+        },
+        onSettled:async ()=>{
+            await queryClient.invalidateQueries(messageKey)
+        }
+      
+    })
   
-
-
-    const onClick = useCallback(() => {
+    const onClick = useCallback(async () => {
 
         if(get().length <= 0) return 
-        
-        socket?.emit('new-message', {to:friendId, from:userId, conversationId:conversation?._id, message:get()})
+
+       const messageData = await mutateAsync({
+            to:friendId,
+            from:userId, 
+            conversationId: conversation?._id, 
+            text:get(),   
+            reply:messageReply,
+            privateRequest,
+            image: inputFile.current.files[0]
+        })
+
+       
+
+        if(!isLoading && !isError){
+            const message = messageData?.data ?? null
+            
+           if(message !== null)  socket?.emit('new-message', {messageId:message?._id, to:friendId, from:userId})
+
+        }
+        clearImagePreview()
         set('')
     }, [get()])
+
+    useEffect(()=>{
+        socket?.on('block-conversation', (block)=>{
+
+            console.log(block)
+            queryClient.setQueryData(['conversation', conversation?._id], (existingData) => {
+           
+                const isBlocked = existingData?.data?.block || false;
+        
+                const updatedData = {
+                  ...existingData,
+                  data: {
+                    ...existingData.data,
+                    block: !isBlocked,
+                  },
+                };
+          
+                return updatedData;
+              });
+        })
+
+        return ()=>{
+            socket.off('block-conversation')
+        }
+    }, [])
+
+
+
 
 
     return (
         <div className='MessageBox-actions-container'>
-             {!!messageReply && (
+            
+            {conversation?.block ? (
+                <>
+                    <div className='conversation-blocked-container'>
+                        <p className='conversation-blocked-text'>Sorry, this conversation has been blocked</p>
+                    </div>
+                </>
+            ) : (
+                <>
+                 {!!messageReply && (
                 <div className='actions-reply-container'>
             
                 <Reply messageId={messageReply}/>
                 <img onClick={deleteMessageReply} className='actions-reply-cross' src={cross} alt="eliminar" />
              </div>
              ) } 
-          
+             <div className="actions-img-send">
+				<img ref={element} onClick={() => clearImagePreview()} />
+			</div>
             <div className='messageBox-actions'>
-            <form className='MessageBox-actions-form'>
+            <div className='MessageBox-actions-form'>
             {store && (
             <AutoComplete
-              placeholder={"Escribe un comentario..."}
+              placeholder={"Aa"}
               rows={1}
-              cols={28}
+              cols={25}
               ref={store}
               set={set}
               stateValue={get}
             />
           )} 
-            </form>
+            </div>
             <div className='MessageBox-actions-InputFile'>
-                <ImgInputFile />
+            <ImgInputFile ref={inputFile} img={agregarPicture} />
             </div>  
+            <img src={emoji} className='MessageBox-emoji' alt="emoji" onClick={()=> setIsOpen((prev)=> !prev)} />
+            <EmojiPickerComponent className={'absolute'} isOpen={isOpen} store={store} set={set}  />
             <button className='MessageBox-sendButton' onClick={() => onClick()}>
-                <img src={sendButton} alt="" />
+              {isLoading && <LoaderVote/> }
+              {!isLoading && (<>
+                {!isLoading && !isError && get()?.length ?  <img src={sendBlue} className='Messagebox-sendbutton'/> :  <img src={sendGray} className='Messagebox-sendbutton'/>}
+              </>)}
+             
             </button>
 
 
                 
             </div>
+                </>
+            ) }
         </div>
     )
 }
