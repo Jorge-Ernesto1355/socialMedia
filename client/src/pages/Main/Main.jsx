a621e021231f391de10545d1bc4b868a00d2b05a
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-28 19:31:00 -0700

    delete category component

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/componentNavbar/Navbar'
-import Perfil from '../../components/LEFT/perfil/Perfil'
-import Sidebar from '../../components/LEFT/Sidebar/Sidebar'
+
 import CreatePost from '../../components/MIDDLE/crearPost/CreatePost'
 import Feed from '../../components/MIDDLE/feed/Feed'
 
-import Category from '../../components/RIGHT/Category/Category'
+
 
 import FriendRequest from '../../components/RIGHT/FriendsRequest/FriendRequest'
 import MessageBoxPage from '../messages/MessagesBoxPages'
 import ConversationView from '../../components/RIGHT/messages/conversationView/ConversationView'
 
+import Sidebar from '../../components/LEFT/Sidebar/Sidebar'
+import UsersOnline from '../../components/RIGHT/messages/usersOnline/UsersOnline'
+
+
 
 const Main = () => {
+  
+
+
     return (
         <main>
             <Navbar></Navbar>
             <div className="container">
                 {/* ===== LEFT ======= */}
                 <div className="left">
-                  
+                  <Sidebar/>
                 </div>
 
                 {/* ===== MIDDLE =====  */}
@@ -31,7 +37,7 @@ const Main = () => {
                 {/* ===== RIGHT =====  */}
                 <div className="rightd">
                     <div className="messages">
-                        <Category />
+                        
                         <ConversationView/>
                         
                     </div>
