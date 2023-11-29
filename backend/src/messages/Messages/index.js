be662c7575b6a0dfc194842cd2b896993e437bac
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-28 19:11:51 -0700

    chore: add controller to index message

--- backend/src/messages/Messages/index.js
+++ backend/src/messages/Messages/index.js
@@ -2,21 +2,25 @@
 const createMessage = require("./mutations/Message");
 const deleteMessage = require("./mutations/DeleteMessage");
 const updateMessage = require("./mutations/updateMessage");
+const markAsRead = require("./mutations/read.js");
 
 //query
 const messages = require("./query/Messages");
 const message = require("./query/message");
 const lastMessage = require("./query/lastMessage");
+const unRead = require("./query/unRead.js");
 
 module.exports = {
   query: {
     messages,
     message,
     lastMessage,
+    unRead,
   },
   mutation: {
     createMessage,
     deleteMessage,
     updateMessage,
+    markAsRead,
   },
 };
