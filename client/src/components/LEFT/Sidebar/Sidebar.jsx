77a1978d44469fa068e8bda5e0c40d816bd7e59e
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-28 19:23:39 -0700

    chore: :lipstick: fix ui to simpler

--- client/src/components/LEFT/Sidebar/Sidebar.jsx
+++ client/src/components/LEFT/Sidebar/Sidebar.jsx
@@ -1,77 +1,44 @@
 import React from "react";
 import "./Sidebar.css";
-
-
-import { Link } from "react-router-dom";
-import Theme from "../../Theme/Theme";
-
-
+import Image from "../../../utilities/Image";
+import friends from './icons/amigos.png'
+import rem from '../../../assets/rem.jpg'
+import group from './icons/grupo.png'
+import messenger  from '../../RIGHT/messages/icons/mensajero.png'
+import marcador from './icons/marcador.png'
 
 const Sidebar = () => {
 
-  const menuItem = document.querySelectorAll(".sidebar a ");
-
-
-  const removeClassList = () => {
-    menuItem.forEach((size) => {
-      size.classList.remove("active");
-    });
-  };
-
-  menuItem.forEach((menu) => {
-    menu.addEventListener("click", () => {
-      removeClassList();
-      menu.classList.toggle("active");
-    });
-  });
-
 
   return (
-    <div className="sidebar">
-      <a className="menu-item">
-        <span>
-        
-        </span>
-        <h3>Home</h3>
-      </a>
+  <ul className="sidebar-container">
 
-      <a className="menu-item">
-        <span>
-        </span>
-        <h3>Explore</h3>
-      </a>
-
-
-      <a className="menu-item" >
-        <span>
-        
-        </span>
-        <i>9</i>
-        <h3>Mensajes</h3>
-      </a>
-      <Link className="menu-item">
-        <span>
-        
-        </span>
-        <h3>Favoritos</h3>
-      </Link>
-
-      <a className="menu-item" >
-        <span>
-         
-        </span>
-        <Theme />
-      </a>
-
-
-      <label
-        htmlFor="create-post"
-
-        className="btn btn-primary"
-      >
-        crear post
-      </label>
-    </div>
+    <li className="sidebar-item">
+    <div className="profile-photo">
+						<Image src={ rem}
+							alt="user"/>
+		</div>
+      <h5 className="sidebar-text">Jorge Ernesto</h5>
+    </li>
+    <li  className="sidebar-item">
+      <img className="sidebar-icon" src={friends} alt="" />
+      <h5 className="sidebar-text">Amigos</h5>
+    </li>
+    <li  className="sidebar-item">
+      <img className="sidebar-icon" src={group} alt="" />
+      <h5 className="sidebar-text">Grupos</h5>
+    </li>
+    <li  className="sidebar-item">
+      <img className="sidebar-icon" src={marcador} alt="" />
+      <h5 className="sidebar-text">Guardados</h5>
+    </li>
+    <li  className="sidebar-item">
+      <img className="sidebar-icon" src={messenger} alt="" />
+      <h5 className="sidebar-text">Messenger</h5>
+    </li>
+    
+  </ul>
+   
   );
 };
 
