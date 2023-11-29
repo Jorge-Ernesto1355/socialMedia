d5fd0c292a0500cf4447366084be9008fd924ec1
JorgeLap <jorgais.ernesto@gmail.com>
2023-11-28 19:14:07 -0700

    fix: :bug: fix error file image
    fix image is undefined in object files

--- backend/src/Post/application/createPost/createPost.js
+++ backend/src/Post/application/createPost/createPost.js
@@ -1,27 +1,24 @@
-
-const {validatePost} = require('../validations/PostSchema')
-const PostService = require('../../PostService')
+const { validatePost } = require("../validations/PostSchema");
+const PostService = require("../../PostService");
 
 const createPost = async (req, res) => {
   // const { description, userId, votes, difusion, postShared, usersTagged } = req.body;
 
+  const result = validatePost(req.body);
 
-    const result = validatePost(req.body)
+  if (result.error) {
+    return res.status(400).json({ error: result.error.message });
+  }
 
-    if (result.error) {
-      return res.status(400).json({ error: result.error.message });
-    }
+  const post = await PostService.create(req);
 
-    const post =  await PostService.create(req)
-    
+  console.log(post);
 
-    if(post.error){
-      return res.status(400).json({ message: post.error.message });
-    }
-   
+  if (post.error) {
+    return res.status(400).json({ message: post.error.message });
+  }
 
-    return res.status(201).json(post);
-  
+  return res.status(201).json(post);
 };
 
 module.exports = createPost;
