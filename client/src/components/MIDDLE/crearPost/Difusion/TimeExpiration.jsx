b2350aa7d8344e00c9e072bc290218f921245f2e
JorgeLap <jorgais.ernesto@gmail.com>
2024-01-26 07:58:13 -0700

    refactor: :zap: change popover from Popover library to Antd library

--- client/src/components/MIDDLE/crearPost/Difusion/TimeExpiration.jsx
+++ client/src/components/MIDDLE/crearPost/Difusion/TimeExpiration.jsx
@@ -3,9 +3,15 @@
 import './TimeExpiration.css'
 import ArrowDown from '../../../../assets/caret.png'
 import CreatePostStore from '../../../../zustand/CreatePostStore'
-import Popover from '../../../../hooks/Popover/Popover'
-import watch from './icons/watch.png'
 
+
+import { Popover, TimePicker } from 'antd'
+import { formatTime } from './utils/formatTime'
+import React from 'react'
+
+const convertHoursToSeconds = (hours) => {
+  return hours * 3600;
+}
 
 const TimeExpiration = () => {
   const { timeExpiration, setTimeExpiration } = CreatePostStore()
@@ -15,40 +21,48 @@ const TimeExpiration = () => {
     setTimeExpiration(time)
   }
 
+  const onChange = (_ , timeString) => {
+  const  [horas, minutos, segundos] = timeString?.split(':').map(Number) ?? 0
+   const  totalSeconds = horas * 3600 + minutos * 60 + segundos;
+   handleTimeExpiration(totalSeconds)
+  };
 
-  const handleHour = (seconds)=>{
-    const hours = Math.floor(seconds / 3600)
-    return hours + ' horas'
-  }
+
+  const content = (
+   <React.Fragment>
+    <h4>Time expiration</h4>
+    <p className='text-muted' style={{fontSize:'x-small'}}>time of the post that will be on public</p>
+    <h5>Default time</h5>
+    <ul className='time-expiration-default-hours'>
+          
+    <li className='time-expiration-item'>
+                <button className='expirePost-text' onClick={()=> handleTimeExpiration(convertHoursToSeconds(5))} >5 H</button>
+            </li>
+            <li className='time-expiration-item'>
+                <button className='expirePost-text' onClick={()=> handleTimeExpiration(convertHoursToSeconds(12))} >12 H</button>
+            </li>
+            <li className='time-expiration-item' >
+                <button className='expirePost-text' onClick={()=> handleTimeExpiration(convertHoursToSeconds(24))} >24 H</button>
+            </li>
+            <li className='time-expiration-item'>
+                <button className='expirePost-text' onClick={()=> handleTimeExpiration(null)}>Always</button>
+            </li>
+          </ul>
+      <h5>Custom time</h5>
+      <TimePicker onChange={onChange} size='small' />
+   </React.Fragment>
+  )
 
 
   return (
-    <div >
-      <Popover trigger={<div className='difusion-container' >
-        <span>{timeExpiration > 0 ?  handleHour(timeExpiration) : 'Always'}</span>
+    <React.Fragment>
+    <Popover trigger={'click'} content={content}>
+      <div className='btn-colored' >
+        <span>{timeExpiration > 0 ?  formatTime(timeExpiration) : 'Always'}</span>
         <img src={ArrowDown} alt="time expiration" />
-      </div>}>
-          <ul className='expirePost-container'>
-          <li className='expirePost-item'>
-               <img src={watch} alt="expire time" className='expirePost-time' />
-            </li>
-            <li className='expirePost-item'>
-                <p className='expirePost-text' onClick={()=> handleTimeExpiration(18000)} >5 Horas</p>
-            </li>
-            <li className='expirePost-item'>
-                <p className='expirePost-text' onClick={()=> handleTimeExpiration(43200)} >12 Horas</p>
-            </li>
-            <li className='expirePost-item' >
-                <p className='expirePost-text' onClick={()=> handleTimeExpiration(86400)} >24 Horas</p>
-            </li>
-            <li className='expirePost-item'>
-                <p className='expirePost-text' onClick={()=> handleTimeExpiration()}>Always</p>
-            </li>
-            
-          </ul>
-        </Popover>
-      
-    </div>
+      </div>
+    </Popover>
+  </React.Fragment>
   )
 }
 
