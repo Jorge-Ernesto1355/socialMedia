9ea2676b456e2ee79d5fff7fe385d14415b74065
JorgeLap <jorgais.ernesto@gmail.com>
2024-01-26 08:09:52 -0700

    feat: :sparkles: popover menu icon
    menu provider to access all functionality application

--- client/src/components/Navbar/menuIcon/MenuIcon.jsx
+++ client/src/components/Navbar/menuIcon/MenuIcon.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import menu from './cuadricula.png'
-import Popover from '../../../hooks/Popover/Popover'
+import './menu-container.css'
+
 import Image from '../../../utilities/Image'
+import SocialMenu from './socialMenu/SocialMenu'
+import CreateMenu from './createMenu/CreateMenu'
+import { Popover } from 'antd'
 const MenuIcon = () => {
+
+  const content = (
+    <React.Fragment>
+       <section className='menu-container'>
+          
+          <h3>Menu</h3>
+            <div className='menu-container-divider'>   
+            <SocialMenu/>
+            <CreateMenu/>
+            </div>
+          </section>
+    </React.Fragment>
+  )
   return (
     <>
 
-        <Popover trigger={<div className='icon-navbar'>
+        <Popover trigger={"click"} content={content} color={"#F3F3F3"}> 
+        
+        <div className='icon-navbar'>
             <Image src={menu}/>
-        </div>}> 
-
-        <ul>
-            hola buenas tardes
-        </ul>
-
+        </div>
+       
         </Popover>
     </>
   )
