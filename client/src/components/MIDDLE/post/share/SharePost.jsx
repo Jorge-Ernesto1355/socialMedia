c7eb6ad0e5692ec21eb26049d2ac717feeae322c
JorgeLap <jorgais.ernesto@gmail.com>
2024-02-10 22:23:26 -0700

    Popover share post

--- client/src/components/MIDDLE/post/share/SharePost.jsx
+++ client/src/components/MIDDLE/post/share/SharePost.jsx
@@ -1,35 +1,45 @@
 import React from 'react'
 import share from '../actionsPost/icons/share.png'
-import Popover from '../../../../hooks/Popover/Popover'
 import copy from '../../../RIGHT/messages/Message/menuMessage/icons/copiar-alt.png'
 import sharePost from './icons/sharePost.png'
 import users from './icons/usuarios.png'
-const SharePost = () => {
-  return (
-    <Popover trigger={
-       <div>
-      <img className="icon" src={share} alt="" />
-      <span className="interaction-button-text">Compartir</span>
-    </div>
-  }>
+import { Popover, message } from 'antd'
+import CopyBoard from 'copy-to-clipboard';
+import './sharePost.css'
+import SendToFriend from './SendToFriend'
 
-<ul className='menuMessageBox-container'>
-            <li className='menuMessageBox-item'>
+
+const SharePost = ({postId, userId}) => {
+
+  
+
+  
+  const copyFunction = (text) => {
+    CopyBoard(text)
+    message.success("copied correctly")
+  }
+
+  const content = (
+    <ul className='sharePost-container'>
+            <li className='menuMessageBox-item' onClick={()=> copyFunction(postId)}>
                 <img  className='menuMessageBox-img' src={copy} alt="" />
                 <p className='menuMessageBox-text'>Copy the link</p>
             </li>
-            <li className='menuMessageBox-item'>
-                <img className='menuMessageBox-img' src={users} alt="" />
-                <p className='menuMessageBox-text'>Compartirlo con un amigo</p>
-            </li>
+            <SendToFriend postId={postId}/>
             <li className='menuMessageBox-item' >
                 <img className='menuMessageBox-img' src={sharePost} alt="" />
-                <p className='menuMessageBox-text'>Compartirlo en tu post</p>
+                <p className='menuMessageBox-text'>Compartirlo en tu feed</p>
             </li>
            
             
           </ul>
-
+  )
+  return (
+    <Popover trigger={"click"} content={content}>
+     <div>
+      <img className="icon" src={share} alt="" />
+      <span className="interaction-button-text">Compartir</span>
+    </div>
     </Popover>
       
   )
