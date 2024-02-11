bc975ce93dec92df3ee118dad3da93c267163934
JorgeLap <jorgais.ernesto@gmail.com>
2024-02-10 22:22:14 -0700

    fix: :art: change Tooltip component

--- client/src/components/RIGHT/Messages/MessageBox/header/MessageBoxHeader.jsx
+++ client/src/components/RIGHT/Messages/MessageBox/header/MessageBoxHeader.jsx
@@ -4,9 +4,10 @@ import rem from '../../../../../assets/rem.jpg'
 import cross from '../../icons/simbolo-x.png'
 import minimizeIcon from '../../icons/menos.png'
 import MenuMessageBox from '../MenuMessageBox/MenuMessageBox'
-import { Tooltip, TooltipContent, TooltipTrigger } from '../../../../../hooks/Popover/Tooltip'
+
 import SimpleLineLoader from '../../../../Loaders/SimpleLineLoader'
 import Image from '../../../../../utilities/Image'
+import { Tooltip } from 'antd'
 const MessageBoxHeader = ({ minimize, deleteConversation, conversation, isLoading, user}) => {
    
     return (
@@ -24,16 +25,16 @@ const MessageBoxHeader = ({ minimize, deleteConversation, conversation, isLoadin
                 </div>
             </div>
             <div className='MessageBox-header-actions'>
-            <Tooltip>
-        <TooltipTrigger> <img className='minimize-icon' src={minimizeIcon} alt="minimze" onClick={() => minimize((prev) => !prev)} /></TooltipTrigger>
-        <TooltipContent>Minimizar</TooltipContent>
-      </Tooltip>
-      <Tooltip>
-        <TooltipTrigger>  <img className='cross-icon' src={cross} alt="delete" onClick={()=> deleteConversation(conversation?._id)} /></TooltipTrigger>
-        <TooltipContent>Eliminar</TooltipContent>
-      </Tooltip>
-               
-               <MenuMessageBox conversation={conversation}/>
+            
+         
+        <Tooltip title={'minimize'}>
+           <img className='minimize-icon' src={minimizeIcon} alt="minimze" onClick={() => minimize((prev) => !prev)} />
+        </Tooltip>
+
+        <Tooltip title={"close window"}>
+                <img className='cross-icon' src={cross} alt="delete" onClick={()=> deleteConversation(conversation?._id)} />
+        </Tooltip>
+            <MenuMessageBox conversation={conversation}/>
             </div>
 
 
