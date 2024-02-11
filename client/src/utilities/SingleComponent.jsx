f0079d28b3c0d32b5709a257a67bbeeb727f9aaf
JorgeLap <jorgais.ernesto@gmail.com>
2024-02-10 22:14:16 -0700

    chore: :fire: delete prop as object
    desestructuring props to use all without use props as a object

--- client/src/utilities/SingleComponent.jsx
+++ client/src/utilities/SingleComponent.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
 
 
-const SingleComponent = (props) => {
+const SingleComponent = ({isError, isLoading, loader, errorMessage, children, isSuccess, successMessage}) => {
 
     return (
         <div className='Component-state-handler-container'>
 
             {/* if IsLoading and isError is false it will display the Loader */}
             <div className='loading-stateManagment'>
-                {props.isLoading && !props.isError && (
-                    props.Loader
+                {isLoading && !isError && !isSuccess &&  (
+                    loader
                 )}
             </div>
 
+            {!isLoading && !isError && !isSuccess && (
+                children
+            )}
+
+
+
             {/* if isLoading is false and isError es false too it will display the children */}
-            {!props.isLoading && !props.isError && (
-                props.children
+            {!isLoading && !isError &&  isSuccess &&   (
+              successMessage
             )}
 
             {/* if there's an error it will display an error message */}
             <div className='error-stateManagment'>
-                {!props.isLoading && props.isError && (
-                    props.ErrorMessageComponent
+                {!isLoading && isError && !isSuccess &&  (
+                errorMessage
                 )}
             </div>
 
