232162a1627b84de547e0dfc6a312c4d02fec5e7
JorgeLap <jorgais.ernesto@gmail.com>
2024-02-10 22:20:26 -0700

    fix: :sparkles: adaptation to new dynamic withSearch HOC

--- client/src/components/RIGHT/Messages/newGroup/Group.jsx
+++ client/src/components/RIGHT/Messages/newGroup/Group.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+/* eslint-disable react/no-children-prop */
+import React, { useCallback, useMemo } from 'react'
 import './Group.css'
 
 import Participants from './Participants/Participants'
 import PropTypes from 'prop-types';
 import UsersWithSearch from '../../../searchFriends/UsesWtihSearch'
 import WithSearch from '../../../../HOCs/WithSearch'
+import GroupStore from '../../../../zustand/GroupStore';
 
 
 const Group = () => {
 
   
     const UsersWithSearchHOC = WithSearch(UsersWithSearch, {INDEX_NAME:'users'})
+    const {addParticipant} = GroupStore()
+
+    const AddParticipant = ({user})=>{
+      if(!user) return 
+      return (
+        <p onClick={()=> addParticipant(user)}>Agregar</p>
+      )
+    }
 
     
   return (
     <section className='group-container' >
         <div className='group-addParticipants'>
             <h4 className='group-title-addParticipant'>Add a participant</h4>
-                <UsersWithSearchHOC description={'Only your friends will appear here'}/>
+                <UsersWithSearchHOC description={'Only your friends will appear here'} >
+                  <AddParticipant/>
+                </UsersWithSearchHOC>
         </div>
         <div className='group-participants'>
                <Participants/>
