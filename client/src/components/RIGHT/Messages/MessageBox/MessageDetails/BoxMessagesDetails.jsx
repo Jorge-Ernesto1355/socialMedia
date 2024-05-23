ed31ccc5abd9e111cae1ad90b46d2ea3d149c782
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-05-23 12:09:12 -0700

    refactor: :sparkles: show success message and error message

--- client/src/components/RIGHT/Messages/MessageBox/MessageDetails/BoxMessagesDetails.jsx
+++ client/src/components/RIGHT/Messages/MessageBox/MessageDetails/BoxMessagesDetails.jsx
@@ -3,20 +3,16 @@ import React from 'react'
 import Image from '../../../../../utilities/Image'
 import rem from '../../../../../assets/rem.jpg'
 import './BoxMessageDetais.css'
-import useMutationRequest from '../../../../../hooks/useMutationRequest'
 import userService from '../../../../../services/UserService'
 import AuthProvider from '../../../../../zustand/AuthProvider'
 import useUserRequest from '../../../../../hooks/auth/useUserRequest'
-import SpinnerLoader from '../../../../../stylesComponents/spinnerLoader/SpinnerLoader'
-
-import HandlerStateButton from '../../../../../utilities/auth/HandlerStateButton'
-import { ToastContainer, toast } from 'react-toastify'
 import { isYourFriend } from '../../../../../utilities/isYourFriend'
-import { useQuery } from 'react-query'
+import { useMutation, useQuery } from 'react-query'
+import { Button, message } from 'antd'
 const BoxMessagesDetails = ({friendUser}) => {
   const {userId} = AuthProvider()
   const privateRequest = useUserRequest()
-  const {mutate, isLoadingMutation, isError, isSuccess}= useMutationRequest(userService.addFriend, {name:'addFriend'})
+  const { mutate, isLoading,  isSuccess} = useMutation(['addFriend', friendUser._id], ()=> userService.addFriend({userId, addUserId: friendUser._id, privateRequest}))
   const { data: userData } = useQuery(["user", userId], () => userService.getUser({ privateRequest, userId, options:['friends'] }));
 
     const user = userData?.data ?? {};
@@ -27,12 +23,10 @@ const BoxMessagesDetails = ({friendUser}) => {
         addUserId: friendUser._id, 
         privateRequest
     }, {
-        onSuccess:(data)=>{
-            if(data?.response?.status === 500){
-               toast.error('Ups... something went wrong')
-            }
-        
+        onSuccess:()=>{
+            message.success("request sended")
         },
+        onError: ()=> message.error("Something went wrong")
         
     })
 }
@@ -47,14 +41,17 @@ const isFriend = isYourFriend(user?.friends, friendUser?._id)
 			<Image src={friendUser?.ProfilePicture ? friendUser?.ProfilePicture : rem} alt="user"/>		
         </div>
         <h5>{friendUser?.username}</h5>
-        <p className='details-description'>{isFriend ? <>{friendUser?.username} is your friend on uniVerse</> : <>{friendUser?.username} is not your friend on uniVerse</>}</p>
+        <p className='details-description'>{isFriend || isSuccess ? <>{friendUser?.username} is your friend on uniVerse</> : <>{friendUser?.username} is not your friend on uniVerse</>}</p>
+        {!isSuccess && (
         <div className='BoxMessageDetails-options'>
-         {!isFriend &&  <button className={`BoxMessageDetails-sendRequest ${isError && 'errorFriendRequest'}`} onClick={()=> handleAddFriend()}>
-           <HandlerStateButton text={'send Request'} isLoading={isLoadingMutation} isError={isError} isSuccess={isSuccess} LoadingMessage={<SpinnerLoader/>} ErrorMessage={<>Error</>} SuccessMessage={'sended request'} />
-       </button>}
+
+         {!isFriend &&  <Button type='primary' loading={isLoading} className={`BoxMessageDetails-sendRequest`} onClick={()=> handleAddFriend()}>
+           Send
+       </Button>}
         <button className='BoxMessageDetails-viewProfile'>View profile</button>
         </div>
-    <ToastContainer/>   
+        )}
+   
     </div>
   )
 }
