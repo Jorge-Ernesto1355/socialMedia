500e7176133927661783192149ecc3d83dbb9856
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-05-23 11:44:13 -0700

    feat: :sparkles: Ellipsi Comment update

--- client/src/components/MIDDLE/post/comments/EllipsiComments/EllipsisComment.jsx
+++ client/src/components/MIDDLE/post/comments/EllipsiComments/EllipsisComment.jsx
@@ -1,28 +1,74 @@
 
-import { motion } from "framer-motion";
+import useUserRequest from "../../../../../hooks/auth/useUserRequest";
+import {  deleteCommentMutation, useHideComment } from "./Actions";
 import "./Ellipsi.css";
-import { variantsMotion } from "../../../../../utilities/variantsMotion";
 
-const EllipsisComment = ({ isOpen }) => {
+
+const EllipsisComment = ({isYours, isYourPost , idComment, postId, editComment}) => {
+
+  const privateRequest = useUserRequest()
+  const {mutate} = deleteCommentMutation({postId, commentId: idComment})
+
+  const hideComment = useHideComment();
   return (
-    <motion.ul
-      variants={variantsMotion}
-      initial={{ scale: 0, opacity: 0 }}
-      animate={`${isOpen ? "show" : "hide"}`}
+
+    <>
+    {isYours && (
+      <ul
       className="ellipsiComment-container"
     >
-
-      <li className="ellipsiComment-item">
-        <h4 className="ellipsiComment-text">Ocultar comentario</h4>
+      <li className="ellipsiComment-item" onClick={()=> mutate({id: idComment, privateRequest})}>
+        <h4 className="ellipsiComment-text">Eliminar comentario</h4>
       </li>
-      <li className="ellipsiComment-item">
-        <h4 className="ellipsiComment-text">Reportar comentario</h4>
-      </li>
-      <li className="ellipsiComment-item">
+      
+      <li className="ellipsiComment-item" onClick={()=> editComment()}>
         <h4 className="ellipsiComment-text">Editar comentario</h4>
       </li>
 
-    </motion.ul>
+    </ul>
+      
+    )}
+
+    {!isYours && isYourPost && (
+        <ul
+        className="ellipsiComment-container"
+      >
+        <li className="ellipsiComment-item" onClick={()=> mutate({id: idComment, privateRequest})}>
+          <h4 className="ellipsiComment-text">Eliminar comentario</h4>
+        </li>
+        
+        <li className="ellipsiComment-item" onClick={()=> hideComment({commentId: idComment, postId, reported: true})}>
+         <h4 className="ellipsiComment-text">reportar comentario</h4>
+       </li>
+       
+  
+      </ul>
+    )}
+
+
+
+    {!isYours && !isYourPost && (
+       <ul
+       className="ellipsiComment-container"
+     >
+       <li className="ellipsiComment-item">
+         <h4 className="ellipsiComment-text" onClick={()=> hideComment({commentId: idComment, postId})}>ocultar comentario</h4>
+       </li>
+       
+       <li className="ellipsiComment-item">
+         <h4 className="ellipsiComment-text" onClick={()=> hideComment({commentId: idComment, postId, reported: true})}>reportar comentario</h4>
+       </li>
+ 
+     </ul>
+    )}
+
+  
+    
+    </>
+
+   
+
+   
   );
 };
 
