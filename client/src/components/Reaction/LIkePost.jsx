e23b2f539255995c585aa60913d75523e03a8d6a
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-05-23 11:50:09 -0700

    feat: make it generraly to all reactions

--- client/src/components/Reaction/LIkePost.jsx
+++ client/src/components/Reaction/LIkePost.jsx
@@ -1,37 +1,30 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./likePost.css";
 import { motion } from "framer-motion";
 import like from "../../assets/likePost.png";
 import { objetsImgs } from "../MIDDLE/post/post/objectImg";
-import useHover from "../../hooks/useHover";
 
 
-const LikePost = ({ reactionType, setShowReactions, isVisible }) => {
+
+const LikePost = ({ reactionType}) => {
 
   
-  const {hovered, show} = useHover()
+
  
-  useEffect(()=>{
-   setShowReactions(show)
-   isVisible()
-  }, [show])
+  
 
   return (
-    <div className="ratingIcon" onMouseEnter={() => hovered(true)}>
+    <div className="ratingIcon" >
       {reactionType === null ? (
         <div
           className="reaction-div"
-          onClick={() => {
-            setShowReactions((prev) => !prev)
-            isVisible()
-          }}
-          onMouseEnter={() => hovered(true)}
+          
         >
           <img className="icon" src={like} alt="like-post" />
           <span className="reaction-text">me gusta</span>
         </div>
       ) : (
-        <div className="actionsActivated" onMouseEnter={() => hovered(true)}>
+        <div className="actionsActivated" >
           <motion.img
             initial={{ scale: 0 }}
             animate={{ scale: 1.1 }}
@@ -40,7 +33,6 @@ const LikePost = ({ reactionType, setShowReactions, isVisible }) => {
           />
           <span
             className={reactionType}
-            onClick={() => hovered(true)}
           >
             me {reactionType}
           </span>
