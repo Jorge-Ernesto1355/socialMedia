b20d195fb2c10c5f1bcbbaabc292bb311c45c163
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-05-23 12:06:35 -0700

    refactor: :fire: refact the code to decline friend

--- client/src/components/RIGHT/FriendsRequest/requestOptions/DeclineFriend.jsx
+++ client/src/components/RIGHT/FriendsRequest/requestOptions/DeclineFriend.jsx
@@ -1,35 +1,24 @@
 import React, { useCallback } from 'react'
 import './DeclineFriend.css'
-import LoaderVote from '../../../MIDDLE/post/Votes/LoaderVote';
 import useUserRequest from '../../../../hooks/auth/useUserRequest';
-import userService from '../../../../services/UserService';
-import useMutationRequest from '../../../../hooks/useMutationRequest';
-import { toast } from 'react-toastify';
-const DeclineFriend = ({ userRequestFriend, currentUser }) => {
+
+const DeclineFriend = ({ userRequestFriend, currentUser, acceptUser}) => {
 
 
     const privateRequest = useUserRequest()
-    const { mutate, isLoadingMutation, isError } = useMutationRequest(userService.acceptFriend, { name: 'requestFriends' })
-
     const handleMutate = useCallback(() => {
-        mutate({
+        acceptUser({
             addUserId: userRequestFriend,
             userId: currentUser,
-            accept: true,
+            accept: false,
             privateRequest
-        }, {
-            onSuccess: () => {
-                toast.success('declined friend request')
-            },
-            onError: () => {
-                toast.error('Something went wrong')
-            }
+            
         })
-    })
+    }, [])
+
     return (
         <button className="Accept-friend" onClick={() => handleMutate()}>
-            {isLoadingMutation && <LoaderVote />}
-            {!isLoadingMutation && <span>Decline</span>}
+            <span>Decline</span>
         </button>
     )
 }
