c4fd11d871451f7966dc86c0e5c35e43925a55a4
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-05-24 11:40:05 -0700

    feat: :sparkles: menu Owner to fit in popover

--- client/src/components/MIDDLE/post/more/ellipsiOwnUser/EllipsiOwner.jsx
+++ client/src/components/MIDDLE/post/more/ellipsiOwnUser/EllipsiOwner.jsx
@@ -1,25 +1,21 @@
-import { motion } from "framer-motion";
-import { variantsMotion } from "../../../../../utilities/variantsMotion";
+
 import EditPostConfirmationModal from "./editPost/EditPostIconConfirmationModal";
 import DeletePost from "./deletePost/DeletePost";
-
 import EditTimeExpiration from "./EditTimeExpiration/EditTimeExpiration";
 
 
-const EllipsiOwner = ({ isOpen, handleCloseEllipsi, username = '', isLoading, postId }) => {
+const EllipsiOwner = ({ handleCloseEllipsi, username = '', isLoading, postId }) => {
 
 
   return (
-    <motion.ul
-      variants={variantsMotion}
-      initial={{ scale: 0, opacity: 0 }}
-      animate={`${isOpen ? "show" : "hide"}`}
+    <ul
       className="ellipsiPost-container"
     >
       <DeletePost postId={postId} />
       <EditPostConfirmationModal postId={postId} handleCloseEllipsi={handleCloseEllipsi} />
       <EditTimeExpiration postId={postId}/>
-    </motion.ul>
+      
+    </ul>
   )
 }
 export default EllipsiOwner
