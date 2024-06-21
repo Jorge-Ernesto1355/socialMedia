5e342e7d68624521d4880afd56a87d3446694523
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-06-20 21:57:13 -0700

    feat: data to paint a background story

---
+++ client/src/components/MIDDLE/Stories/modalStories/StoryText/utils/backgroundsGradient.js
@@ -0,0 +1,79 @@
+export const BackgroundsGradients = [
+    {
+      id: 1,
+      name: 'Background 1',
+      style: {
+        backgroundImage: 'linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)'
+      }
+    },
+    {
+      id: 2,
+      name: 'Background 2',
+      style: {
+        backgroundImage: 'linear-gradient(to top, #fad0c4 0%, #ffd1ff 100%)'
+      }
+    },
+    {
+      id: 3,
+      name: 'Background 3',
+      style: {
+        backgroundImage: 'linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%)'
+      }
+    },
+    {
+      id: 4,
+      name: 'Background 4',
+      style: {
+        backgroundImage: 'linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%)'
+      }
+    },
+    {
+      id: 5,
+      name: 'Background 5',
+      style: {
+        backgroundImage: 'linear-gradient(to right, #4facfe 0%, #00f2fe 100%)'
+      }
+    },
+    {
+      id: 6,
+      name: 'Background 6',
+      style: {
+        backgroundImage: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)'
+      }
+    },
+    {
+      id: 7,
+      name: 'Background 7',
+      style: {
+        backgroundImage: 'linear-gradient(to right, #b8cbb8 0%, #b8cbb8 0%, #b465da 0%, #cf6cc9 33%, #ee609c 66%, #ee609c 100%)'
+      }
+    },
+    {
+      id: 8,
+      name: 'Background 8',
+      style: {
+        backgroundImage: 'linear-gradient(to right, #eea2a2 0%, #bbc1bf 19%, #57c6e1 42%, #b49fda 79%, #7ac5d8 100%)'
+      }
+    },
+    {
+      id: 9,
+      name: 'Background 9',
+      style: {
+        backgroundImage: 'linear-gradient(to top, #c471f5 0%, #fa71cd 100%)'
+      }
+    },
+    {
+      id: 10,
+      name: 'Background 10',
+      style: {
+        backgroundImage: 'linear-gradient(to top, #dbdcd7 0%, #dddcd7 24%, #e2c9cc 30%, #e7627d 46%, #b8235a 59%, #801357 71%, #3d1635 84%, #1c1a27 100%)'
+      }
+    },
+    {
+      id: 11,
+      name: 'Background 11',
+      style: {
+        backgroundImage: 'linear-gradient(to top, #7028e4 0%, #e5b2ca 100%)'
+      }
+    },
+  ];
