3b3e7883657c4f3f7b0d4917b69e7987a8440188
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-07-10 15:14:08 -0700

    feat: Refactor total points
    minimize the error when reactions is not found

--- backend/src/reaction/utils/getTotalPoints.js
+++ backend/src/reaction/utils/getTotalPoints.js
@@ -3,35 +3,26 @@ const isValidObjectId = require("../../libs/isValidObjectId");
 const labelsValue = require("./labelsValue");
 
 async function getTotalPoints(reactions) {
-  // Verifica si reactions es un array y no está vacío
   if (!Array.isArray(reactions) || reactions.length === 0) {
-    return 0
+    return 0;
   }
 
   let puntosTotales = 0;
 
-  // Recorre el array de IDs de reacciones
+  const queryOptions = {
+    model: "Reaction",
+    select: ['label']
+  };
+
   for (const reactionId of reactions) {
-     
-
     try {
-      // Busca la reacción en la base de datos por su ID
-      const queryOptions = {
-        model:"Reaction", 
-        select:['label']
+      const reaction = await isValidObjectId({_id: reactionId}, queryOptions);
+      if (!reaction.error) {
+        puntosTotales += labelsValue[reaction.label] || 0;
       }
-      const reaction = await isValidObjectId({_id:reactionId}, queryOptions)
-    
-      if(reaction.error)
-        throw new Error(reaction.message)
-
-      puntosTotales += labelsValue[reaction.label] ? labelsValue[reaction.label] : 0
-
+      
     } catch (error) {
-      return {
-        error, 
-        message:error.message
-      }
+      return error
     }
   }
 
