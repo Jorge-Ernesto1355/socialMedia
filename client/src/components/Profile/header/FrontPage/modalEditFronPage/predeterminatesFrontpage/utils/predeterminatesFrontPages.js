ecc13a6a6b39a1f7ae5275a2be8f2308aa97846e
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-07-10 15:30:16 -0700

    front pages for default

--- client/src/components/Profile/header/FrontPage/modalEditFronPage/predeterminatesFrontpage/utils/predeterminatesFrontPages.js
+++ client/src/components/Profile/header/FrontPage/modalEditFronPage/predeterminatesFrontpage/utils/predeterminatesFrontPages.js
@@ -1,42 +1,33 @@
 export const predeterminatesFrontPages = [
     {
         id: 1,
-        url: "https://res.cloudinary.com/cbta/image/upload/v1716708275/prueba/quk16gvfs93syrlu0kb1.jpg", 
-        public_id: "prueba/xgvwnsd7m1xkimueirtb"
+        url: "https://res.cloudinary.com/cbta/image/upload/v1/images/ddtipnoaynkwcrpiywrt", 
+        public_id: "images/ddtipnoaynkwcrpiywrt", 
+        previewUrl:"https://res.cloudinary.com/cbta/image/upload/f_auto,q_auto:low/v1/images/ddtipnoaynkwcrpiywrt" 
     },
     {
         id: 2,
-        url: "https://res.cloudinary.com/cbta/image/upload/v1716772784/prueba/xgvwnsd7m1xkimueirtb.jpg", 
-        public_id: "prueba/xgvwnsd7m1xkimueirtb"
-    },
+        url: "https://res.cloudinary.com/cbta/image/upload/v1/images/pfsytylv5ucv4fbrfbvb", 
+        public_id: "images/pfsytylv5ucv4fbrfbvb", 
+        previewUrl: "https://res.cloudinary.com/cbta/image/upload/f_auto,q_auto:low/v1/images/pfsytylv5ucv4fbrfbvb"
+    }, 
     {
         id: 3,
-        url: "https://res.cloudinary.com/cbta/image/upload/v1716772784/prueba/xgvwnsd7m1xkimueirtb.jpg", 
-        public_id: "prueba/xgvwnsd7m1xkimueirtb"
-    },
+        url: "https://res.cloudinary.com/cbta/image/upload/v1/images/x1h6opppjexhexqhglsc", 
+        public_id: "images/x1h6opppjexhexqhglsc", 
+        previewUrl: "https://res.cloudinary.com/cbta/image/upload/f_auto,q_auto:low/v1/images/x1h6opppjexhexqhglsc"
+    }, 
     {
         id: 4,
-        url: "https://res.cloudinary.com/cbta/image/upload/v1716772784/prueba/xgvwnsd7m1xkimueirtb.jpg", 
-        public_id: "prueba/xgvwnsd7m1xkimueirtb"
-    },
+        url: "https://res.cloudinary.com/cbta/image/upload/v1/images/moggryftj6alo1v9aevh", 
+        public_id: "images/moggryftj6alo1v9aevh", 
+        previewUrl: "https://res.cloudinary.com/cbta/image/upload/f_auto,q_auto:low/v1/images/moggryftj6alo1v9aevh"
+    }, 
     {
         id: 5,
-        url: "https://res.cloudinary.com/cbta/image/upload/v1716772784/prueba/xgvwnsd7m1xkimueirtb.jpg", 
-        public_id: "prueba/xgvwnsd7m1xkimueirtb"
-    },
-    {
-        id: 6,
-        url: "https://res.cloudinary.com/cbta/image/upload/v1716772784/prueba/xgvwnsd7m1xkimueirtb.jpg", 
-        public_id: "prueba/xgvwnsd7m1xkimueirtb"
-    },
-    {
-        id: 7,
-        url: "https://res.cloudinary.com/cbta/image/upload/v1716772784/prueba/xgvwnsd7m1xkimueirtb.jpg", 
-        public_id: "prueba/xgvwnsd7m1xkimueirtb"
-    },
-    {
-        id: 8,
-        url: "https://res.cloudinary.com/cbta/image/upload/v1716772784/prueba/xgvwnsd7m1xkimueirtb.jpg", 
-        public_id: "prueba/xgvwnsd7m1xkimueirtb"
-    },
+        url: "https://res.cloudinary.com/cbta/image/upload/v1/images/nrwilneyp2zbi6fziqlo", 
+        public_id: "images/nrwilneyp2zbi6fziqlo", 
+        previewUrl: "https://res.cloudinary.com/cbta/image/upload/f_auto,q_auto:low/v1/images/nrwilneyp2zbi6fziqlo"
+    }, 
 ]
+
