40941f6d1840097fac593666a293ef9f6c877b58
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-07-10 15:19:21 -0700

    feat(security): :sparkles: add property TTL
    add ExpiresIn to time to live and hastTags

--- backend/src/story/model/Story.js
+++ backend/src/story/model/Story.js
@@ -6,10 +6,14 @@ const Story = new Schema({
     text: { type: String, required: true },
     media: { url: String, previewUrl:  String, public_id: String, resourceType: String, background: String }, // URL to media file if any
     createdAt: { type: Date, default: Date.now },
-    expiresIn: { type: Date, required: true}
+    expiresIn: { 
+        type: Date, 
+        index: { expires: 0 }, 
+        default: null 
+      }, 
+    hashTags:[String]
 })
 
-Story.index({ expiresIn: 1 }, { expireAfterSeconds: 0 });
 
 Story.plugin(mongoosePaginate)
 
