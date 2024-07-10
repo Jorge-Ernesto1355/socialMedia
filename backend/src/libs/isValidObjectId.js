6128f0b0134ecf3158c8499921edf3e61aaa8470
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-07-10 15:03:37 -0700

    refactor: :sparkles: refact funcion to accept populate option

--- backend/src/libs/isValidObjectId.js
+++ backend/src/libs/isValidObjectId.js
@@ -6,15 +6,31 @@ module.exports = async function validateObjectId(query, options) {
     if (typeof options !== "object")
       throw new Error("options is not an object");
 
-    if (!mongoose.models[options.model]) throw new Error("model not found");
+    if (!mongoose.models[options?.model])
+      throw new Error("model not found");
 
-    const document = await mongoose.models[options.model]
-      .findOne(query)
-      .select(options.select);
+    const model = mongoose.models[options.model];
+    let queryBuilder = model.findOne(query);
 
-    if (document) return document;
+    if (options.select) {
+      queryBuilder = queryBuilder.select(options.select);
+    }
+
+    if (options.populate) {
+      if (Array.isArray(options.populate)) {
+        options.populate.forEach(populateOption => {
+          queryBuilder = queryBuilder.populate(populateOption);
+        });
+      } else if (typeof options.populate === 'object') {
+        queryBuilder = queryBuilder.populate(options.populate);
+      }
+    }
+
+    const document = await queryBuilder.exec();
 
     if (!document) throw new DocumentNotFound("document not found");
+
+    return document
   } catch (error) {
     return {
       error,
