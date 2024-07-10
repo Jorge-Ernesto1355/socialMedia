907c1aa60a86e8081d6fe54de44a529a4a628fc2
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-07-10 15:30:39 -0700

    feat(infrastructure): :sparkles: interests object to know

---
+++ client/src/components/MIDDLE/feed/customizedFeed/utils/interests.js
@@ -0,0 +1,17 @@
+export const interests = [
+    { "id": 1, "name": "Programming", "suggestions": ["JavaScript", "Python", "Java", "C++", "Ruby", "Go", "Swift", "React", "Astro","Css"] },
+    { "id": 2, "name": "Design", "suggestions": ["Graphic Design", "UI/UX", "Photoshop", "Illustrator", "Figma", "Sketch"] },
+    { "id": 3, "name": "Music", "suggestions": ["Guitar", "Piano", "Drums", "Violin", "Bass", "Singing", "Songwriting"] },
+    { "id": 4, "name": "Cooking", "suggestions": ["Italian", "Mexican", "Japanese", "Vegan", "Baking", "Grilling"] },
+    { "id": 5, "name": "Sports", "suggestions": ["Soccer", "Basketball", "Tennis", "Running", "Swimming", "Cycling"] },
+    { "id": 6, "name": "Travel", "suggestions": ["Adventure", "Beach", "Mountain", "Road Trip", "Cruise", "Cultural"] },
+    { "id": 7, "name": "Photography", "suggestions": ["Portrait", "Landscape", "Wildlife", "Street", "Macro", "Sports"] },
+    { "id": 8, "name": "Writing", "suggestions": ["Fiction", "Non-fiction", "Poetry", "Blogging", "Screenwriting", "Journaling"] },
+    { "id": 9, "name": "Technology", "suggestions": ["AI", "Machine Learning", "Blockchain", "Cybersecurity", "IoT", "Cloud Computing"] },
+    { "id": 10, "name": "Fitness", "suggestions": ["Yoga", "Pilates", "CrossFit", "Bodybuilding", "Cardio", "Strength Training"] },
+    { "id": 11, "name": "Gaming", "suggestions": ["PC Gaming", "Console Gaming", "Mobile Gaming", "eSports", "Retro Gaming", "VR Gaming"] },
+    { "id": 12, "name": "Movies", "suggestions": ["Action", "Comedy", "Drama", "Horror", "Sci-Fi", "Documentary"] },
+    { "id": 13, "name": "Reading", "suggestions": ["Novels", "Biographies", "Self-help", "Science Fiction", "Fantasy", "Historical"] },
+    { "id": 14, "name": "Gardening", "suggestions": ["Vegetable Gardening", "Flower Gardening", "Indoor Plants", "Herb Gardening", "Landscaping"] },
+    { "id": 15, "name": "Crafts", "suggestions": ["Knitting", "Crochet", "Scrapbooking", "Woodworking", "Jewelry Making"] }
+  ]
