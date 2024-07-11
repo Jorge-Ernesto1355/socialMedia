0147a9b0c8a17f65ad03c07dfee314c892c17f50
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-07-10 22:11:11 -0700

    refactor: :sparkles: more reusable feed post
    change all to allow any service to manage a differens service

--- client/src/components/MIDDLE/feed/Feed.jsx
+++ client/src/components/MIDDLE/feed/Feed.jsx
@@ -12,6 +12,8 @@ import PostServices from "../post/services/PostServices";
 import AuthProvider from "../../../zustand/AuthProvider";
 import ErrorPost from "../post/post/ErrorPost/ErrorPost";
 import Skeleton from "../../Skeleton/Skeleton";
+import ComponentStateHandler from "../../../hooks/stateManagmentComponent/ComponentStateHandler";
+import CustomizedFeed from "./customizedFeed/CustomizedFeed";
 
 
 /**
@@ -19,33 +21,26 @@ import Skeleton from "../../Skeleton/Skeleton";
  * @param {number} limit - The maximum number of posts to fetch per page.
  * @returns {JSX.Element} - The rendered feed of posts.
  */
-export default function Feed() {
+export default function Feed({service = ()=>{}, name="", userId =""}) {
+
 
   const privateRequest = useUserRequest()
-  const { userId } = AuthProvider()
   const uuid = uuidv4()
-
-
 
   const { results, isLoading, isError,  hasNextPage, fetchNextPage, reset, refetch } =
     useInfiniteScroll({
-      name: "posts",
-      request: PostServices.getTimeLine,
+      name: name,
+      request: service,
       privateRequest,
       id: userId
     });
+
 
   useEffect(() => {
     if (userId) {
       refetch();
     }
   }, [userId, refetch]);
-
-  if (isError) {
-    return <ErrorPost reset={reset} />;
-  }
-  
- 
 
 
   return (
@@ -55,13 +50,13 @@ export default function Feed() {
       loader={<PostLoader />}
       next={() => fetchNextPage()}
     >
-      <ul className="feeds" key={uuid}>
-        {results?.map((post) => (
-          <>
-            <Post key={post._id} post={post} />
-          </>
-        ))}
-      </ul>
+      <ComponentStateHandler isLoading={isLoading} isError={isError} Loader={<PostLoader/>} EmptyMessage={<CustomizedFeed refetch={refetch}/>} ErrorMessageComponent={<ErrorPost refetch={refetch}/> } items={results}>
+        <ul className="feeds" key={uuid}>
+          {results?.map((post) => (
+              <Post key={post._id} post={post} />
+          ))}
+        </ul>
+      </ComponentStateHandler>
     </InfiniteScroll>
   );
 }
