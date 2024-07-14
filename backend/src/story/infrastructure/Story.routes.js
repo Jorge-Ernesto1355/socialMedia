6ed1788fad4b6cd1041e8bd72960580e22c84955
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-07-13 20:34:56 -0700

    feat(infrastructure): :sparkles: add new controller to story route
    add controller feed stories

--- backend/src/story/infrastructure/Story.routes.js
+++ backend/src/story/infrastructure/Story.routes.js
@@ -10,5 +10,7 @@ router.get("/storiesByUser/:userId", index.query.getStoriesByUser)
 
 router.get("/storiesFromUser/:userId", index.query.getStoriesFromUser)
 
+router.get("/feed/:userId", index.query.feedStories)
+
 
 module.exports = router
