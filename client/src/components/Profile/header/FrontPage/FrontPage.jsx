82285da5be61d33ff66fc4d97a9ffefce895ee02
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-07-17 13:41:57 -0700

    refactor: :sparkles: add cover picture of user

--- client/src/components/Profile/header/FrontPage/FrontPage.jsx
+++ client/src/components/Profile/header/FrontPage/FrontPage.jsx
@@ -5,16 +5,19 @@ import ModalEditFrontPage from './modalEditFronPage/ModalEditFrontPage';
 import { useQuery } from 'react-query';
 import UserService from '../../../../services/UserService';
 import useUserRequest from '../../../../hooks/auth/useUserRequest';
+import { Skeleton } from 'antd';
 const FrontPage = ({userId}) => {
 
   const privateRequest = useUserRequest()
   const { data: user, isLoading: isLoadingUser } = useQuery(["user", userId], () => UserService.getUser({ privateRequest, userId , options: ["coverPicture"]}));
+
+
   return (
     <div className='profile-card-frontPage'>
-        <img className='profile-card-fronPage-img' src={montaña} alt="" />
+       {user?.coverPicture?.url ? <img className='profile-card-fronPage-img' src={user?.coverPicture?.url} alt="front page" /> : <Skeleton.Image rootClassName='root-skeleton-coverpicture' style={{width: '100%', height: '100%'}} /> }
         <ModalEditFrontPage userId={userId} user={user} isLoadingUser={isLoadingUser}/>
     </div>
   )
 }
 
-export default FrontPage
+export default FrontPage 
