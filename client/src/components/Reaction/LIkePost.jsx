70c2790c1466f2e7015093e6e74ddb6f61775015
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-07-17 13:32:44 -0700

    feat: :sparkles: reaction to post with reactions

--- client/src/components/Reaction/LIkePost.jsx
+++ client/src/components/Reaction/LIkePost.jsx
@@ -4,21 +4,19 @@ import "./likePost.css";
 import like from "../../assets/likePost.png";
 import { objetsImgs } from "../MIDDLE/post/post/objectImg";
 
-
-
-const LikePost = ({ reactionType}) => {
-
+const DisplayLike = ()=>{
   
+}
 
- 
-  
 
-  return (
-    <div className="ratingIcon" >
-      {reactionType === null ? (
+
+const LikePost = ({ reactionType, reactionUser}) => {
+
+  if(typeof reactionUser === "undefined"){
+    return (<>
+    {reactionType === null  ? (
         <div
           className="reaction-div"
-          
         >
           <img className="icon like" src={like} alt="like-post" />
           <span className="reaction-text">me gusta</span>
@@ -38,8 +36,26 @@ const LikePost = ({ reactionType}) => {
         </div>
       )
       }
-    </div >
+    </>)
+  } else {
+  return (
+        <div className="actionsActivated" >
+          <img
+          className="reacted-icon"
+            src={objetsImgs[reactionUser?.reaction?.label]}
+            alt="reactiontype-likeSelected"
+          />
+          <span
+            className={reactionUser?.reaction?.label}
+          >
+            me {reactionUser?.reaction?.label}
+          </span>
+        </div>
   )
+      
+      
+  }
+
 }
 
 export default LikePost;
