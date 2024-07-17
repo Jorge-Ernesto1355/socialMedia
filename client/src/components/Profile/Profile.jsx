bd590e03a1682b6d21a00d4d9b4d6db79c7b1c3b
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-07-17 13:47:43 -0700

    feat: :sparkles: manage profile main to manage all footer options

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React  from "react";
+import React, { useState }  from "react";
 import "./Profile.css";
 import { useParams } from "react-router-dom"
 import CrearPost from "../MIDDLE/crearPost/CreatePost";
@@ -8,40 +8,70 @@ import profileProvider from "./ProfileProvider";
 import Details from "./Details/Details";
 import Photos from "./photos/Photos";
 import Friends from "./Friends/Friends";
+import UserService from "../../services/UserService";
+import { objectFooterProfile } from "./header/Footer";
+import StoriesProfile from "./storiesProfile/StoriesProfile";
+import PostReactioned from "./postReactioned/PostReactioned";
 
+
+
+const ProfileLayout = ({ userId, children }) => (
+  <div className="Profile">
+    <Header userId={userId} />
+    <div className="body">
+      <div className="left">
+        <Details userId={userId} />
+        <Photos userId={userId} />
+        <Friends userId={userId} />
+      </div>
+      <div className="profile-right">
+        {children}
+      </div>
+    </div>
+  </div>
+);
 
 const Profile = () => {
-
-  const {tab} = profileProvider()
+  const { tab } = profileProvider();
   const params = useParams();
   const userId = params.userId;
 
-  return (
-    <div className="Profile">
-     <Header userId={userId}/>
+  const renderContent = () => {
+    switch (tab) {
+      case objectFooterProfile.usersPosts:
+        return (
+            <Feed userId={userId} service={UserService.getUserPosts} name={"usersPosts"} />
+        );
+      case objectFooterProfile.favorites:
+        return (
+          <Feed userId={userId} service={UserService.getFavoritesPosts} name={"favoritesPosts"} />
+        );
+      case objectFooterProfile.about:
+        return (
+          <>
+            <Details userId={userId} />
+            <Photos userId={userId} />
+            <Friends userId={userId} />
+          </> 
+        );
+        case objectFooterProfile.stories:
+        return (
+           <StoriesProfile userId={userId}/>
+        );
+        case objectFooterProfile.reactions:
+          return (
+             <Feed userId={userId} service={UserService.getPostsReaction} name={"reactedPosts"} />
+          );
+      // Puedes añadir más casos aquí para otras pestañas
+      default:
+        return <div>Default content</div>;
+    }
+  };
 
-      <div className="body">
-        <div className="left">
-           <div>
-              <Details userId={userId} />
-              <Photos userId={userId}/>
-              <Friends userId={userId}/>
-            </div>
-        </div>
-        <div className="profile-right">
-        {tab === 1 ? (
-              <>
-                <CrearPost />
-                <Feed  type={"userPosts"} />
-              </>
-            ) : 
-            (
-              <>hola</>
-            )
-          }
-        </div>
-      </div>
-    </div>
+  return (
+    <ProfileLayout userId={userId}>
+      {renderContent()}
+    </ProfileLayout>
   );
 };
 
