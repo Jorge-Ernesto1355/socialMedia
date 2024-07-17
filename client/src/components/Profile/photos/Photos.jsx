88108f5b2f4601eda4d6598d9dc7e716f177b6d2
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-07-17 13:33:53 -0700

    refactor: :sparkles: refact code from photos componnet
    more flexible to loader and error message

--- client/src/components/Profile/photos/Photos.jsx
+++ client/src/components/Profile/photos/Photos.jsx
@@ -6,20 +6,23 @@ import useUserRequest from "../../../hooks/auth/useUserRequest";
 import { useCallbackRequest } from "../../../hooks/useCallbackRequest/useCallbackRequest";
 import { Button, Flex, Image, Typography } from "antd";
 import { SkeletonsSquare } from "../Loader/SkeletonSquare";
+import useInfiniteScroll from "../../../hooks/useInfiniteScroll/useInfiniteScroll";
+import ComponentStateHandler from "../../../hooks/stateManagmentComponent/ComponentStateHandler";
+import InfiniteScroll from "react-infinite-scroll-component";
+import BlurImageLoader from "../../../utilities/BlurImageLoader";
 
 const { Text, Title} = Typography;
 const Photos = ({ userId}) => {
  
  
-const privateRequest = useUserRequest()
-  const { data, isLoading, isError} =
-    useCallbackRequest({
-      name: "photos",
-      request: UserService.getPhotos,
-      privateRequest,
-      id: userId
-    });
-
+  const privateRequest = useUserRequest()
+  const { results, isLoading, isError,  hasNextPage, fetchNextPage, reset, refetch } =
+  useInfiniteScroll({
+    name: "posts",
+    request: UserService.getPhotos,
+    privateRequest,
+    id: userId
+  });
 
     
    
@@ -36,24 +39,24 @@ const privateRequest = useUserRequest()
                See all photos
             </Button>
         </Flex>
-        <ul className="photos-body">
-            
-             {isLoading &&  <SkeletonsSquare/>}
-           {!isLoading && 
-             <>
-               {data?.data?.map((img, index) => (
-                <li  className={`photos-item${index}`} key={img?.public_id}>
-                  <Image
-                  style={{width: "100%", height: "100%"}}
-                  wrapperClassName="wrapper-photo"
-                  className={"photos-item-img"}
-                  src={img?.url}
-                />
-                </li>
-              ))}
-             </>
-           }
-        </ul>
+        <InfiniteScroll
+          dataLength={results.length}
+          hasMore={hasNextPage || isLoading}
+          loader={< SkeletonsSquare/>}
+          next={() => fetchNextPage()}
+        >
+          <ComponentStateHandler isLoading={isLoading} isError={isError} Loader={<SkeletonsSquare/>} EmptyMessage={"no hay fotos"} ErrorMessageComponent={"error putos"} items={results}>
+          <ul className="photos-body">
+                  {results?.map((img, index) => (
+                  <li  className={`photos-item${index}`} key={img?.public_id}>
+                     <BlurImageLoader preview={img.previewUrl} divStyleClass={"imageProfile-blurImage-container"} image={img.url} notImage={false} alt={"posible image profile"}/>
+                  </li>
+                ))}
+          </ul>
+          </ComponentStateHandler>
+    </InfiniteScroll>
+       
+      
         
     </div>
   );
