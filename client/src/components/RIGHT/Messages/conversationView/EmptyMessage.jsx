0902ddae9083d6ca91ba1ba06f0e4574429724b0
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-07-17 13:31:44 -0700

    emptyMessage component

--- client/src/components/RIGHT/Messages/conversationView/EmptyMessage.jsx
+++ client/src/components/RIGHT/Messages/conversationView/EmptyMessage.jsx
@@ -1,12 +1,29 @@
-import { Button, Empty } from 'antd'
+import { Button, Empty, Flex, Typography } from 'antd'
 import React from 'react'
+import UserMaybeKnow from '../../../userMaybeKnow/UserMaybeKnow'
+const { Text, Title} = Typography;
+const description = (
+  <>
+  <Flex style={{width: "100%"}}>
+    <Text style={{width: "100%"}} type='secondary'>
+      <Flex justify='center' gap={5}>
+      Upps... try to 
+          <UserMaybeKnow>
+              <p style={{color: "var(--color-blue)", cursor: "pointer"}}>connect</p>
+           </UserMaybeKnow>
+       with your people 
+      </Flex>
+    </Text>
+  </Flex>
+  </>
+)
 
 const EmptyMessage = () => {
   return (
     
-    <Empty description={<p className='text-muted'>Upps... try to connect with your people</p>}>
-    <Button type='primary' size='default' >Connect</Button>
-    </Empty>
+    <Empty description={description}/>
+
+   
     
   )
 }
