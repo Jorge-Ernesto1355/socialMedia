d6d2ac0d43c043f4763e3fb3cbbb67566fb55f32
Jorge Ernesto <jorgais.ernesto@gmail.com>
2024-07-17 13:35:54 -0700

    add infinite scroll to modal profile picture

--- client/src/components/Profile/header/modalProfilePicture/ModalProfilePicture.jsx
+++ client/src/components/Profile/header/modalProfilePicture/ModalProfilePicture.jsx
@@ -6,11 +6,16 @@ import './modalProfilePicture.css'
 import { useCallbackRequest } from '../../../../hooks/useCallbackRequest/useCallbackRequest';
 import SpinnerLoader from '../../../../stylesComponents/spinnerLoader/SpinnerLoader';
 import { Loading3QuartersOutlined, PlusOutlined } from '@ant-design/icons';
-import { Upload,  Divider, Modal, Typography, message} from 'antd';
+import { Upload,  Divider, Modal, Typography, message, Flex, Empty} from 'antd';
 import { beforeUpload } from './util/beforeUpload';
 import { getBase64 } from './util/getBase64';
 import ImgCrop from 'antd-img-crop';
 import ImageToProfilePicture from './Image/ImageToProfilePicture';
+import useInfiniteScroll from '../../../../hooks/useInfiniteScroll/useInfiniteScroll';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import ComponentStateHandler from '../../../../hooks/stateManagmentComponent/ComponentStateHandler';
+import LoaderStories from '../../../MIDDLE/Stories/LoaderStories/LoaderStories';
+import { SkeletonsSquare } from '../../Loader/SkeletonSquare';
 
 
 const { Text, Title} = Typography;
@@ -70,14 +75,17 @@ const ModalProfilePicture = ({children, userId}) => {
       
       
       const privateRequest = useUserRequest()
-      const { data, isLoading} =
-        useCallbackRequest({
-          name: "photos",
-          request: UserService.getPhotos,
-          privateRequest,
-          id: userId
-        });
+      const { results, isLoading, isError,  hasNextPage, fetchNextPage, reset, refetch } =
+      useInfiniteScroll({
+        name: "photosProfilePicture",
+        request: UserService.getPhotos,
+        privateRequest,
+        id: userId
+      });
 
+      
+
+        
         const uploadButton = (
           <button
             style={{
@@ -100,7 +108,7 @@ const ModalProfilePicture = ({children, userId}) => {
   return (
     <>
         {React.cloneElement(children, {onClick: ()=> showModal()})}
-      <Modal style={{width: "900px"}} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+      <Modal style={{width: "900px"}} open={isModalOpen} onOk={ handleOk} footer={null} onCancel={handleCancel}>
       
         <div className='modal-profile-header'>
             <Title style={{ marginBottom: "0px"}}  level={4}>Upload your profile image</Title>
@@ -137,19 +145,25 @@ const ModalProfilePicture = ({children, userId}) => {
        <Divider plain>OR</Divider>
        <Title style={{marginTop:"20px", marginBottom: "0px"}} level={4}>Choose from your gallery</Title>
        <Text >Photos that already you uploaded</Text>
-      <ul className='modal-profile-photos'>
-      {isLoading &&  <SpinnerLoader/>}
-           {!isLoading && 
-             <>
-               {data?.data?.map((img) => (
-                <ImageToProfilePicture key={img._id} img={img} setImageUrl={setImageUrl} userId={userId}/>
-              ))}
-             </>
-           }
-      </ul>
+
+       <InfiniteScroll
+          dataLength={results.length}
+          hasMore={hasNextPage || isLoading}
+          loader={<LoaderStories />}
+          next={() => fetchNextPage()}
+        >
+          <ComponentStateHandler isLoading={isLoading} isError={isError} Loader={<SkeletonsSquare/>} EmptyMessage={<Flex style={{width:"100%"}} justify='center'><Empty></Empty></Flex>} ErrorMessageComponent={<Text type='danger'>Upps, something went wrong, try to comunicate with us or reload the page</Text>} items={results}>
+                <ul className='modal-profile-photos'>
+                        {results?.map((img) => ( <ImageToProfilePicture key={img._id} img={img} setImageUrl={setImageUrl} userId={userId}/>   ))}
+               </ul>
+          </ComponentStateHandler>
+    </InfiniteScroll>
+      
       </Modal>
     </>
   )
 }
 
 export default ModalProfilePicture
+
+
